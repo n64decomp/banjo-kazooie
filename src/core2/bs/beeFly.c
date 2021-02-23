48a73f50022b7597beb636a2ccf5ee0b118ff269
Mittenz <michaelsh12391@gmail.com>
2021-02-22 21:59:35 -0600

    decomp func_802A0590

--- src/core2/bs/beeFly.c
+++ src/core2/bs/beeFly.c
@@ -4,9 +4,18 @@
 
 f32 func_8034A754(f32, f32);
 void func_80354030(f32*, f32);
+void func_8028A274(s32, f32);
 
+f32  func_8029B2E8(void);
+void func_802991B4(f32);
+f32 func_8029B33C(void);
+void func_8029797C(f32);
+f32 func_80299228(void);
+void func_80297970(f32);
+void func_80297BEC(f32);
 
-//#pragma GLOBAL_ASM("asm/nonmatchings/core2/bs/beeFly/func_802A04F0.s")
+extern  u8 D_8037D2C0;
+
 void func_802A04F0(void){
     f32 plyrPos[3]; //sp1C
 
@@ -20,7 +29,19 @@ void func_802A04F0(void){
 
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/core2/bs/beeFly/func_802A0590.s")
+//#pragma GLOBAL_ASM("asm/nonmatchings/core2/bs/beeFly/func_802A0590.s")
+void func_802A0590(void){
+    func_802A0340();
+    func_8028A274(0x1df, 1.5f);
+    func_8029C7F4(1,1,3,6);
+    if(func_8029B2E8() != 0.0f){
+        func_802991B4(func_8029B33C());
+    }
+    func_8029797C(func_80299228());
+    func_80297970(0.0f);
+    func_80297BEC(-1200.0f);
+    D_8037D2C0 = 0;
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/core2/bs/beeFly/func_802A0630.s")
 
