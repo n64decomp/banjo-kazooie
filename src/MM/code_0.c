9d347d3133acb2a809723ab56be78b4ccd6d7d45
ThatCowGuy <cray40@gmx.de>
2021-02-23 03:04:19 +0100

    func_80386744 and func_80387F44

--- src/MM/code_0.c
+++ src/MM/code_0.c
@@ -2,11 +2,13 @@
 #include "functions.h"
 #include "variables.h"
 
-
 #pragma GLOBAL_ASM("asm/nonmatchings/MM/code_0/func_803863F0.s")
 
 #pragma GLOBAL_ASM("asm/nonmatchings/MM/code_0/func_80386444.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/MM/code_0/func_80386744.s")
+//#pragma GLOBAL_ASM("asm/nonmatchings/MM/code_0/func_80386744.s")
+void func_80386744(s32 arg0, s32 arg1) {
+    func_80386444(arg1);
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/MM/code_0/func_80386768.s")
