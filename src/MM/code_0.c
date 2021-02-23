ef265057e0aef7059be152f89e13f9b72c9c6715
ThatCowGuy <cray40@gmx.de>
2021-02-23 03:05:55 +0100

    func_80386744 and func_80387F44

--- src/MM/code_0.c
+++ src/MM/code_0.c
@@ -6,7 +6,6 @@
 
 #pragma GLOBAL_ASM("asm/nonmatchings/MM/code_0/func_80386444.s")
 
-//#pragma GLOBAL_ASM("asm/nonmatchings/MM/code_0/func_80386744.s")
 void func_80386744(s32 arg0, s32 arg1) {
     func_80386444(arg1);
 }
