20cbb5ead57cbf927da7e167a599b009f7bbecf1
Mittenshugg <michaelsh12391@gmail.com>
2021-03-05 19:38:12 -0600

    func_8030BEDC

--- src/core2/gc/transition.c
+++ src/core2/gc/transition.c
@@ -1,7 +1,14 @@
 #include <ultra64.h>
 #include "functions.h"
 #include "variables.h"
-
+struct{
+    u8 pad0[0x4];
+    s32 unk4;
+    u8 unk8;
+    u8 pad9[3]; //not needed
+    void *unkC;
+    f32 rotation;
+} D_80382430;
 
 #pragma GLOBAL_ASM("asm/nonmatchings/core2/gc/transition/func_8030B400.s")
 
@@ -29,6 +36,13 @@
 
 #pragma GLOBAL_ASM("asm/nonmatchings/core2/gc/transition/func_8030BEA4.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/core2/gc/transition/func_8030BEDC.s")
+//#pragma GLOBAL_ASM("asm/nonmatchings/core2/gc/transition/func_8030BEDC.s")
+void func_8030BEDC(void){
+    D_80382430.unk4 = 0;
+    D_80382430.unk8 = 0;
+    D_80382430.unkC = NULL;
+    D_80382430.rotation = 0.0f;
+    func_8030B498(0,0);
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/core2/gc/transition/func_8030BF1C.s")
