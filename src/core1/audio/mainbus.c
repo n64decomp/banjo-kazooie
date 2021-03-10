f1fbbcc2ec84c7eb3bb6f83851b4db956119ed4d
Mittenshugg <michaelsh12391@gmail.com>
2021-03-10 16:25:16 -0600

    core1/audio/mainbus.c

--- src/core1/audio/mainbus.c
+++
@@ -1,8 +0,0 @@
-#include <ultra64.h>
-#include "functions.h"
-#include "variables.h"
-
-
-#pragma GLOBAL_ASM("asm/nonmatchings/core1/audio/mainbus/alMainBusParam.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/core1/audio/mainbus/alMainBusPull.s")
