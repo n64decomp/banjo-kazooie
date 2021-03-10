7f6191c0a9de104ad4a816ea46457ae9f589e8b8
Mittenshugg <michaelsh12391@gmail.com>
2021-03-09 21:27:29 -0600

    libaudio labeling

---
+++ src/core1/audio/auxbus.c
@@ -0,0 +1,8 @@
+#include <ultra64.h>
+#include "functions.h"
+#include "variables.h"
+
+
+#pragma GLOBAL_ASM("asm/nonmatchings/core1/audio/auxbus/alAuxBusParam.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/core1/audio/auxbus/alAuxBusPull.s")
--- src/core1/code_33D60.c
+++
@@ -1,8 +0,0 @@
-#include <ultra64.h>
-#include "functions.h"
-#include "variables.h"
-
-
-#pragma GLOBAL_ASM("asm/nonmatchings/core1/code_33D60/alAuxBusParam.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/core1/code_33D60/func_802717B0.s")
