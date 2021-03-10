d47ad06f7fbea437feed58707e57ea28cef5c3c9
Mittenshugg <michaelsh12391@gmail.com>
2021-03-10 17:45:36 -0600

    core1/audio/resample

--- src/core1/audio/resample.c
+++
@@ -1,8 +0,0 @@
-#include <ultra64.h>
-#include "functions.h"
-#include "variables.h"
-
-
-#pragma GLOBAL_ASM("asm/nonmatchings/core1/audio/resample/alResampleParam.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/core1/audio/resample/func_8027156C.s")
--- src/core1/code_26C70.c
+++ src/core1/code_26C70.c
@@ -5,4 +5,4 @@
 
 #pragma GLOBAL_ASM("asm/nonmatchings/core1/code_26C70/func_80264690.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/core1/code_26C70/func_802646A0.s")
+#pragma GLOBAL_ASM("asm/nonmatchings/core1/code_26C70/osVirtualToPhysical.s")
