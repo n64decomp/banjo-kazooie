41130e69ff388a26d1bf57177ae40eae0439a747
Mittenshugg <michaelsh12391@gmail.com>
2021-03-10 16:23:25 -0600

    core1/audio/save.c

--- src/core1/audio/save.c
+++
@@ -1,8 +0,0 @@
-#include <ultra64.h>
-#include "functions.h"
-#include "variables.h"
-
-
-#pragma GLOBAL_ASM("asm/nonmatchings/core1/audio/save/alSaveParam.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/core1/audio/save/alSavePull.s")
