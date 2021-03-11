6de8680fe7dfecb8d1c4ec8faaf5dfe52096623a
Mittenshugg <michaelsh12391@gmail.com>
2021-03-11 15:38:43 -0600

    core1/audio/n_synstopvoice.c

---
+++ src/core1/done/audio/n_synstopvoice.c
@@ -0,0 +1,20 @@
+#include <ultra64.h>
+#include "n_synth.h"
+
+
+void    n_alSynStopVoice( N_ALVoice *v)
+{
+    ALParam  *update;
+    
+    if (v->pvoice) {
+        
+        update = __n_allocParam();
+        ALFailIf(update == 0, ERR_ALSYN_NO_UPDATE);
+
+        update->delta  = D_80276E84->synth.paramSamples + v->pvoice->offset;
+        update->type   = AL_FILTER_STOP_VOICE;
+        update->next   = 0;
+
+        func_802607C0(v->pvoice, AL_FILTER_ADD_UPDATE, update);        
+    }
+}
