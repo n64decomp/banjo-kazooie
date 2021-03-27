f1ae94d15fbe1a33c0a1311e7f4f039e1373796d
Isaac Miell <isaacjmiell@gmail.com>
2021-03-27 15:03:30 +1030

    Red to green colour gradient for progress badges

--- tools/progress_read.py
+++ tools/progress_read.py
@@ -5,6 +5,13 @@ import sys
 import csv
 import anybadge
 
+def RGB_to_hex(RGB):
+    ''' [255,255,255] -> "#FFFFFF" '''
+    # Components need to be integers for hex to make sense
+    RGB = [int(x) for x in RGB]
+    return "#"+"".join(["0{0:x}".format(v) if v < 16 else
+                        "{0:x}".format(v) for v in RGB])
+
 def main(csv_name, version, overlay):
     with open(csv_name, mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
@@ -22,10 +29,13 @@ def main(csv_name, version, overlay):
                     done_byte += int(row['length'])
         percent = ((done_byte/total_byte) * 100)
         print("bytes: %3.4f%% (%d/%d), funcs: %3.4f%% (%d/%d)" % (percent, done_byte, total_byte,((done_func/total_func) *100), done_func, total_func ))
+        green = min(255, round(min(1, (percent / 100) * 2) * 256))
+        red = min(255, round(min(1, ((100 - percent) / 100) * 2) * 256))
+        color = RGB_to_hex([red, green, 0])
         if overlay == 'total':
-            badge = anybadge.Badge("Banjo-Kazooie (us.v10)", "%3.4f%%" % (percent))
+            badge = anybadge.Badge("Banjo-Kazooie (us.v10)", "%3.4f%%" % (percent), default_color=color)
         else:
-            badge = anybadge.Badge(overlay, "%3.4f%%" % (percent))
+            badge = anybadge.Badge(overlay, "%3.4f%%" % (percent), default_color=color)
         badge.write_badge('progress_' + overlay + '.svg',overwrite=True)
 
 
