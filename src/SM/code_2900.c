9db8319be7472caaac03fe223f006ea97622746d
banjo.decomp <banjo.decomp@gmail.com>
2021-04-11 17:05:47 -0500

    SM/code_2900.c done

--- src/SM/code_2900.c
+++ src/SM/code_2900.c
@@ -2,7 +2,18 @@
 #include "functions.h"
 #include "variables.h"
 
+void func_8034DEB4(Actor *, f32);
 
-#pragma GLOBAL_ASM("asm/nonmatchings/SM/code_2900/func_80388CF0.s")
+/* .code */
+void func_80388CF0(void){
+    if(getGameMode() != game_mode_attract_demo){
+        func_8034DEB4(func_8034C528(0x1F2), 0.0f);
+        func_8034DEB4(func_8034C528(0x1F3), -5000.0f);
+    }
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/SM/code_2900/func_80388D48.s")
+void func_80388D48(void){
+    if(*(u32*)PHYS_TO_K1(0x200) - PHYS_TO_K1(0xC290000)){
+        func_80388CF0();
+    }
+}
