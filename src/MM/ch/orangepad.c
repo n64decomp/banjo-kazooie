ad5ea333cb0c8c1c825cd525c26fea5db3ee1c7a
banjo.decomp <banjo.decomp@gmail.com>
2021-04-12 23:44:25 -0500

    MM: func_803863F0()

--- src/MM/ch/orangepad.c
+++ src/MM/ch/orangepad.c
@@ -16,21 +16,14 @@ ActorInfo chorangepadInfo = { 0x66, actor_orange_pad, model_orange_pad, 0, NULL,
     {0,0,0,0}, 0.0f, {0,0,0,0}
 };
 
-#if NONMATCHING
+/*.code */
 void func_803863F0(s32 x, s32 y, s32 z){
     f32 pos[3];
-    f32 tmp;
 
-    tmp = x;
-    pos[1] = y;
-    pos[0] = tmp;
-    pos[2] = z;
+    TUPLE_ASSIGN(pos, x, y, z);
     
     jiggySpawn(jiggy_mm_orange_pads, pos);
 }
-#else
-#pragma GLOBAL_ASM("asm/nonmatchings/MM/ch/orangepad/func_803863F0.s")
-#endif
 
 #pragma GLOBAL_ASM("asm/nonmatchings/MM/ch/orangepad/func_80386444.s")
 
