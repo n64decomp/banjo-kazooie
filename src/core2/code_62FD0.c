b86381409598b5b601dd382e2dca2c62f95b5440
banjo.decomp <banjo.decomp@gmail.com>
2021-04-23 17:02:20 -0500

    func_802E9FEC()

--- src/core2/code_62FD0.c
+++ src/core2/code_62FD0.c
@@ -18,7 +18,16 @@ struct20s * func_802E9F9C(struct1Fs *arg0, s32 arg1){
     return 0;
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/core2/code_62FD0/func_802E9FEC.s")
-// int func_802E9FEC(void * arg0, s32 arg1, s32 arg2){
+int func_802E9FEC(struct1Fs * arg0, s32 arg1, void *arg2){
+    s32 i;
+    struct20s *v0 = func_802E9F9C(arg0, arg1);
 
-// }
+    if(v0){
+        for(i = 0; i < v0->count; i++){
+            if(v0->data[i] == *(s16 *)arg2){
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
