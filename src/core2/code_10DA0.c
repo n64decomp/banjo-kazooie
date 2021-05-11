fb11303e3f4d8516d038b6c276122973877f1c26
banjo.decomp <banjo.decomp@gmail.com>
2021-05-11 16:16:36 -0500

    player_applyIdealPitch()

--- src/core2/code_10DA0.c
+++ src/core2/code_10DA0.c
@@ -19,7 +19,9 @@ void player_setPitch(f32 pitch_deg){
     D_8037C540 = mlNormalizeAngle(pitch_deg);
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/core2/code_10DA0/func_80297F84.s")
+void player_applyIdealPitch(void){
+    D_8037C540 = D_8037C544;
+}
 
 f32 player_getPitch(void){
     return D_8037C540;
