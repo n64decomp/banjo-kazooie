1ac72cabde8e47ae44fafad85ad96a3d85da0fa5
Mr-Wiseguy <mrwiseguyromhacking@gmail.com>
2021-05-21 21:44:48 -0400

    18.8272%, some more libultra io files

--- src/core1/io/conteepread.c
+++
@@ -1,8 +0,0 @@
-#include <ultra64.h>
-#include "functions.h"
-#include "variables.h"
-
-
-#pragma GLOBAL_ASM("asm/nonmatchings/core1/io/conteepread/osEepromRead.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/core1/io/conteepread/__osPackEepReadData.s")
