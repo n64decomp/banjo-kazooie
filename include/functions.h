d79d2fc7dfa91a7d2724f7a9106910a982658ae9
banjo.decomp <banjo.decomp@gmail.com>
2021-05-24 14:27:06 -0500

    func_802C8A54()

--- include/functions.h
+++ include/functions.h
@@ -124,6 +124,7 @@ void actor_collisionOff(Actor *);
 void *assetcache_get(s32 arg0);
 
 Actor *actor_new(s32 (* position)[3], s32 yaw, ActorInfo *actorInfo, u32 flags);
+Actor *func_802C8A54(s32 (* position)[3], s32 yaw, ActorInfo* actorInfo, u32 flags);
 
 Actor *marker_getActor(ActorMarker *);
 
--- src/core2/code_41460.c
+++ src/core2/code_41460.c
@@ -2,6 +2,14 @@
 #include "functions.h"
 #include "variables.h"
 
+extern s32 D_8037DD90;
+extern s32 D_8037DD94;
+extern struct{
+    u32 unk0;
+    u32 unk4;
+    u32 unk8;
+    u32 unkC;
+} D_8037DD98;
 
 #pragma GLOBAL_ASM("asm/nonmatchings/core2/code_41460/func_802C83F0.s")
 
@@ -9,7 +17,15 @@
 
 #pragma GLOBAL_ASM("asm/nonmatchings/core2/code_41460/func_802C8580.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/core2/code_41460/func_802C8A54.s")
+Actor *func_802C8A54(s32 (* position)[3], s32 yaw, ActorInfo* actorInfo, u32 flags){
+    D_8037DD90 = 0xF;
+    D_8037DD94 = 1;
+    D_8037DD98.unk0 = 0xFA;
+    D_8037DD98.unk4 = 0xFA;
+    D_8037DD98.unk8 = 0xFA;
+    D_8037DD98.unkC = 0x78;
+    return func_802C8580(position, yaw, actorInfo, flags);
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/core2/code_41460/func_802C8AA8.s")
 
