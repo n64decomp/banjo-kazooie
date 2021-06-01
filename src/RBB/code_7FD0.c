5846154522374c7cc374e32f68dc460a0fbcccae
banjo.decomp <banjo.decomp@gmail.com>
2021-06-01 03:06:33 -0500

    RBB/code_5F10 done

--- src/RBB/code_7FD0.c
+++ src/RBB/code_7FD0.c
@@ -2,6 +2,15 @@
 #include "functions.h"
 #include "variables.h"
 
+/* typedefs and declarations */
+void func_8038E3D8(Actor *this);
+
+/* .data */
+extern ActorInfo D_80390D50; /* = {
+    0x1ab, 0x297, 0x43C, 0x0, NULL,
+    func_8038E3D8, NULL, func_80325888,
+    {0, 0, 0, 0}, 0.0f, {0,0,0,0}
+};*/
 
 #pragma GLOBAL_ASM("asm/nonmatchings/RBB/code_7FD0/func_8038E3C0.s")
 
