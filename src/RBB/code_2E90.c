742df8aee4f952da63177a18638e6d423614e435
banjo.decomp <banjo.decomp@gmail.com>
2021-06-07 13:21:30 -0500

    RBB/code_2E90.c

--- src/RBB/code_2E90.c
+++ src/RBB/code_2E90.c
@@ -2,40 +2,208 @@
 #include "functions.h"
 #include "variables.h"
 
-// 0000 A140: 01 BB 00 00 00 00 00 00  44 20 5C CD C4 AF 00 00
-// 0000 A150: 00 00 00 00 00 00 00 00  42 B4 00 00 00 00 00 00
-// 0000 A160: 00 00 00 00 43 B4 00 00  00 00 00 00 00 00 00 00
-// 0000 A170: 43 87 00 00 00 00 00 00  00 00 00 00 42 B4 00 00
-// 0000 A180: 40 80 00 00 
-//                        01 BC 00 00  C4 48 00 00 44 20 5C CD
-// 0000 A190: C5 16 00 00 42 BE 00 00  00 00 00 00 00 00 00 00
-// 0000 A1A0: 43 BE 00 00 00 00 00 00  00 00 00 00 43 8E 80 00
-// 0000 A1B0: 00 00 00 00 00 00 00 00  42 B4 00 00 00 00 00 00
-// 0000 A1C0: 00 00 00 00 40 88 00 00  
-//                                     01 BD 00 00 44 48 00 00
-// 0000 A1D0: 44 20 5C CD C5 16 00 00  42 C8 00 00 00 00 00 00
-// 0000 A1E0: 00 00 00 00 43 C8 00 00  00 00 00 00 00 00 00 00
-// 0000 A1F0: 43 96 00 00 00 00 00 00  00 00 00 00 42 B4 00 00
-// 0000 A200: 00 00 00 00 00 00 00 00  40 90 00 00 
-//                                                 00 00 00 00
-// 0000 A210: 00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00
-// 0000 A220: 00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00
-// 0000 A230: 00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00
-// 0000 A240: 00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00
+/* typedefs and declarations */
+typedef struct {
+    s16 uid;
+    f32 unk4[3];
+    f32 unk10[3];
+    f32 unk1C[3];
+    f32 unk28[3];
+    f32 unk34[3];
+    f32 unk40;
+}Struct_RBB_2E90_0;
 
-// 0000 A250: 01 91 01 BB 04 0F 00 00  00 00 00 00 80 38 94 4C
-// 0000 A260: 00 00 00 00 80 32 58 88  00 00 00 00 00 00 00 00
-// 0000 A270: 00 00 00 00 
-//                        01 92 01 BC  04 10 00 00 00 00 00 00
-// 0000 A280: 80 38 94 4C 00 00 00 00  80 32 58 88 00 00 00 00
-// 0000 A290: 00 00 00 00 00 00 00 00  
-//                                     01 93 01 BD 04 11 00 00
-// 0000 A2A0: 00 00 00 00 80 38 94 4C  00 00 00 00 80 32 58 88
-// 0000 A2B0: 00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00
+typedef struct {
+    f32 unk0[3];
+    Struct_RBB_2E90_0 *unkC;
+    f32 unk10[3];
+    f32 unk1C[3];
+    f32 unk28;
+}ActorLocal_RBB_2E90;
 
-//D_80390640
-#pragma GLOBAL_ASM("asm/nonmatchings/RBB/code_2E90/func_80389280.s")
+void func_8038944C(Actor *this);
 
-#pragma GLOBAL_ASM("asm/nonmatchings/RBB/code_2E90/func_803892D8.s")
 
+/* .data */
+Struct_RBB_2E90_0 D_80390530[] = {
+    { 
+        0x1BB, 
+        {0.0f, 641.45f, -1400.0f}, 
+        {0.0f, 0.0f,  90.0f},
+        {0.0f, 0.0f, 360.0f}, 
+        {0.0f, 0.0f, 270.0f}, 
+        {0.0f, 0.0f,  90.0f}, 
+        4.0f
+    },
+    { 
+        0x1BC, 
+        {-800.0f, 641.45f, -2400.0f},
+        { 95.0f, 0.0f, 0.0f},
+        {380.0f, 0.0f, 0.0f}, 
+        {285.0f, 0.0f, 0.0f}, 
+        { 90.0f, 0.0f, 0.0f}, 
+        4.25f
+    },
+    { 
+        0x1BD, 
+        {800.0f, 641.45f, -2400.0f}, 
+        {100.0f, 0.0f, 0.0f},
+        {400.0f, 0.0f, 0.0f}, 
+        {300.0f, 0.0f, 0.0f}, 
+        {90.0f, 0.0f, 0.0f}, 
+        4.5f
+    },
+    {0}
+};
+
+ActorInfo D_80390640 = {
+    0x191, 0x1BB, 0x40F, 0x0, NULL,
+    func_8038944C, NULL, func_80325888,
+    {0, 0, 0, 0}, 0.0f, {0,0,0,0}
+};
+
+ActorInfo D_80390664 = {
+    0x192, 0x1BC, 0x410, 0x0, NULL,
+    func_8038944C, NULL, func_80325888,
+    {0, 0, 0, 0}, 0.0f, {0,0,0,0}
+};
+
+ActorInfo D_80390688 = {
+    0x193, 0x1BD, 0x411, 0x0, NULL,
+    func_8038944C, NULL, func_80325888,
+    {0, 0, 0, 0}, 0.0f, {0,0,0,0}
+};
+
+/* .code */
+Struct_RBB_2E90_0 * func_80389280(Actor *this){
+    Struct_RBB_2E90_0 * iPtr;
+    for(iPtr = D_80390530; iPtr->uid != 0; iPtr++){
+        if(iPtr->uid == this->modelCacheIndex)
+            return iPtr;
+    }
+    return NULL;
+}
+
+void func_803892D8(Actor *this, s32 arg1){
+    f32 pad;
+    ActorLocal_RBB_2E90 *local = (ActorLocal_RBB_2E90 *)&this->local;
+    this->unk10_31 = arg1;
+
+    local->unk28 = 0.0f;
+    if(this->unk10_31 == 1){
+        local->unk0[2] = 0.0f;
+        local->unk0[1] = 0.0f;
+        local->unk0[0] = 0.0f;
+        ml_vec3f_copy(&local->unk10, &local->unkC->unk1C);
+        ml_vec3f_copy(&local->unk1C, &local->unkC->unk1C);
+    }//L8038935C
+
+    if(this->unk10_31 == 2){
+        ml_vec3f_copy(&local->unk0, &local->unkC->unk28);
+        ml_vec3f_copy(&local->unk1C, &local->unkC->unk10);
+        local->unk28 = local->unkC->unk40;
+    }//L803893A8
+
+    if(this->unk10_31 == 3){
+        ml_vec3f_copy(&local->unk0, &local->unkC->unk28);
+        ml_vec3f_copy(&local->unk1C, &local->unkC->unk1C);
+        local->unk28 = local->unkC->unk40;
+    }//L803893F4
+
+    if(this->unk10_31 == 4){
+        ml_vec3f_copy(&local->unk0, &local->unkC->unk34);
+        ml_vec3f_copy(&local->unk10, &local->unkC->unk1C);
+        local->unk1C[2] = 0.0f;
+        local->unk1C[1] = 0.0f;
+        local->unk1C[0] = 0.0f;
+    }
+}
+
+#ifndef NONMATCHING
 #pragma GLOBAL_ASM("asm/nonmatchings/RBB/code_2E90/func_8038944C.s")
+#else
+void func_8038944C(Actor *this){
+    ActorLocal_RBB_2E90 *local = (ActorLocal_RBB_2E90 *)&this->local;
+    f32 sp50[3];
+    f32 sp44[3];
+    int i;
+    f32 tick = func_8033DD9C();
+
+    if(!this->unk16C_4){
+        this->marker->propPtr->unk8_3 = 1;
+        this->unk16C_4 = TRUE;
+        local->unkC = func_80389280(this);
+        if(this->unk10_31 == 0){
+            ml_vec3f_copy(&this->position, &local->unkC->unk4);
+        }
+        local->unk0[2] = 0.0f;
+        local->unk0[1] = 0.0f;
+        local->unk0[0] = 0.0f;
+
+        local->unk10[2] = 0.0f;
+        local->unk10[1] = 0.0f;
+        local->unk10[0] = 0.0f;
+
+        local->unk1C[2] = 0.0f;
+        local->unk1C[1] = 0.0f;
+        local->unk1C[0] = 0.0f;
+
+        local->unk28 = 0.0f;
+        func_803892D8(this, 1);
+    }//L80389508
+
+    if(func_8025773C(&local->unk28, tick)){
+        if(this->unk10_31 == 2)
+            func_803892D8(this, 3);
+        else if(this->unk10_31 == 3)
+            func_803892D8(this, 2);
+    }//L80389568
+
+    switch (this->unk10_31)
+    {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+        sp50[0] = this->pitch;
+        sp50[1] = this->yaw;
+        sp50[2] = this->roll;
+        this->pitch += local->unk10[0] * tick;
+        this->yaw += local->unk10[1] * tick;
+        this->roll += local->unk10[2] * tick;
+
+        sp44[0] = this->pitch;
+        sp44[1] = this->yaw;
+        sp44[2] = this->roll;
+
+        for(i = 0; i < 3; i++){
+            if( (sp50[i] < 90.0f && 90.0f <= sp44[i])
+                || (sp50[i] < 270.0f && 270.0f <= sp44[i])
+                || (sp50[i] < 180.0f && 180.0f <= sp44[i])
+                || (sp50[i] < 360.0f && 360.0f <= sp44[i])
+            ){
+                func_8030E8B4(0x33367002, &this->position, 0x03e80032);
+            }
+        }
+        this->pitch += (360.0f <= this->pitch)? -360 : 0;
+        this->yaw += (360.0f <= this->yaw)? -360 : 0;
+        this->roll += (360.0f <= this->roll)? -360 : 0;
+
+        for(i = 0; i < 3; i++){//L803897B8
+            if( local->unk10[i] < local->unk1C[i] ){
+                local->unk10[i] += local->unk0[i]*tick;
+                local->unk10[i] = MIN(local->unk1C[i] , local->unk10[i]);
+            }
+            else if( local->unk1C[i] < local->unk10[i]){
+                local->unk10[i] -= local->unk0[i]*tick;
+                local->unk10[i] = MAX(local->unk1C[i] , local->unk10[i]);
+            }
+        }
+        break;
+    }//L80389860
+
+    if(this->unk10_31 == 1 && mapSpecificFlags_get(0)){
+        func_803892D8(this, 2);
+    }
+}
+#endif
--- src/RBB/code_34B0.c
+++ src/RBB/code_34B0.c
@@ -2,6 +2,10 @@
 #include "functions.h"
 #include "variables.h"
 
+/* .data */
+// 0000 A2C0: 01 94 01 BE 04 12 00 00  00 00 00 00 80 38 99 C0
+// 0000 A2D0: 00 00 00 00 80 32 58 88  00 00 00 00 00 00 00 00
+// 0000 A2E0: 00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00
 
 #pragma GLOBAL_ASM("asm/nonmatchings/RBB/code_34B0/func_803898A0.s")
 
