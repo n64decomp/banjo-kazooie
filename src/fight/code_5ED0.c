79048fd28f027f927da709b12a89f76de37828f4
banjo.decomp <banjo.decomp@gmail.com>
2021-06-15 13:03:27 -0500

    fight/code_5ED0

--- src/fight/code_5ED0.c
+++ src/fight/code_5ED0.c
@@ -4,16 +4,39 @@
 
 extern void func_80386654(f32, void *, void *);
 
+
+extern f32 D_803918D0;
 extern f32 D_80391918;
 extern s32 D_80391948;
 extern s32 D_80391958;
 
+
+extern f64 D_80392480;
+extern f64 D_80392488;
 extern f32 D_80392490;
 
 extern f32 D_80392520;
 
-void func_8038C2C0(f32 (*)[3], s32, enum asset_e, f32);
-#pragma GLOBAL_ASM("asm/nonmatchings/fight/code_5ED0/func_8038C2C0.s")
+/* .code */
+void func_8038C2C0(f32 (*arg0)[3], s32 arg1, enum asset_e id, f32 arg3){
+    Actor * temp_s0 = func_802F0BD0(arg1);
+    f32 sp24;
+
+    func_802EF950(temp_s0, id);
+    func_802EFA90(temp_s0, 1, 6);
+    func_802EFB54(temp_s0, arg0);
+    func_802EFE24(temp_s0, -300.0f, -300.0f, -300.0f, 300.0f, 300.0f, 300.0f);
+    func_802EFB70(temp_s0, arg3*D_80392480, arg3*D_80392488);
+    func_802EFB84(temp_s0, 0.0f, 0.0f);
+    func_802EF9F8(temp_s0, 0.5f);
+    func_802EFA18(temp_s0, 3);
+    func_802EFE5C(temp_s0, 0.0f, 0.01f);
+    func_802EFEC0(temp_s0, 3.0f, 3.3f);
+    func_802EFA5C(temp_s0, 0.0f, 0.35f);
+    func_802EFD7C(temp_s0, &D_803918D0);
+    func_802EFA70(temp_s0, 4);
+    func_802EF5C8(temp_s0, arg1);
+}
 
 void func_8038C424(f32 (*arg0)[3], s32 arg1, enum asset_e id, f32 arg3){
     Actor * temp_s0 = func_802F0BD0(arg1);
