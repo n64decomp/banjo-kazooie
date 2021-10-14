372aac401d169d32f1d87822b08235dfbcfa2c78
Mittenz <michaelsh12391@gmail.com>
2021-10-13 22:14:23 -0500

    core2/code_C3F0.c

--- src/core2/code_C3F0.c
+++ src/core2/code_C3F0.c
@@ -2,15 +2,40 @@
 #include "functions.h"
 #include "variables.h"
 
+void func_802933FC(s32 arg0);
 
-#pragma GLOBAL_ASM("asm/nonmatchings/core2/code_C3F0/func_80293380.s")
+/*.bss */
+extern u8 D_8037C1D0[0x1C];
 
-#pragma GLOBAL_ASM("asm/nonmatchings/core2/code_C3F0/func_802933C0.s")
+/* code */
+void func_80293380(void){
+    int i;
+    for(i = 1; i < 0x1C; i++){
+        func_802933FC(i);
+    }
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/core2/code_C3F0/func_802933D0.s")
+int func_802933C0(s32 arg0){
+    return D_8037C1D0[arg0];
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/core2/code_C3F0/func_802933E8.s")
+int func_802933D0(s32 arg0){
+    return !D_8037C1D0[arg0];
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/core2/code_C3F0/func_802933FC.s")
+void func_802933E8(s32 arg0){
+    D_8037C1D0[arg0] = TRUE;
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/core2/code_C3F0/func_8029340C.s")
+void func_802933FC(s32 arg0){
+    D_8037C1D0[arg0] = FALSE;
+}
+
+void func_8029340C(s32 arg0){
+    if(D_8037C1D0[arg0]){
+        D_8037C1D0[arg0] = FALSE;
+    }
+    else {
+        D_8037C1D0[arg0] = TRUE;
+    }
+}
