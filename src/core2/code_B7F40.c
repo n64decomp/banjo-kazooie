857114b15d6451dd6f7dcf2d4a2a7a157dd6f845
banjo.decomp <banjo.decomp@gmail.com>
2021-11-16 16:17:13 -0600

    core2/code_B7F40.c done

--- src/core2/code_B7F40.c
+++ src/core2/code_B7F40.c
@@ -2,8 +2,17 @@
 #include "functions.h"
 #include "variables.h"
 
+void func_8033EF08(void);
+void func_8033EF10(void);
 
-#pragma GLOBAL_ASM("asm/nonmatchings/core2/code_B7F40/func_8033EED0.s")
+/* .data */
+void *D_80371E60 = NULL;
+
+/* .code */
+void func_8033EED0(void){
+    func_8033EF10();
+    func_8033EF08();
+}
 
 void func_8033EEF8(void){}
 
@@ -13,18 +22,33 @@ void func_8033EF08(void){}
 
 void func_8033EF10(void){}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/core2/code_B7F40/func_8033EF18.s")
+void func_8033EF18(s32 arg0, s32 arg1){
+    return;
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/core2/code_B7F40/func_8033EF24.s")
+void func_8033EF24(void){
+    if(D_80371E60){
+        free(D_80371E60);
+    }
+    D_80371E60 = NULL;
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/core2/code_B7F40/func_8033EF58.s")
+void func_8033EF58(void){
+    D_80371E60 = malloc(0x50);
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/core2/code_B7F40/func_8033EF7C.s")
+void func_8033EF7C(s32 arg0){
+    return;
+}
 
 void func_8033EF84(void){}
 
 void func_8033EF8C(void){}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/core2/code_B7F40/func_8033EF94.s")
+void func_8033EF94(s32 arg0){
+    return;
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/core2/code_B7F40/func_8033EF9C.s")
+void func_8033EF9C(s32 arg0){
+    return;
+}
