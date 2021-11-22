215cfe65c15acae37cd4b4848a5bda753bcddb72
banjo.decomp <banjo.decomp@gmail.com>
2021-11-22 15:08:13 -0600

    CC/ch/tooth.c done

--- src/CC/code_1120.c
+++
@@ -1,8 +0,0 @@
-#include <ultra64.h>
-#include "functions.h"
-#include "variables.h"
-
-
-#pragma GLOBAL_ASM("asm/nonmatchings/CC/code_1120/func_80387510.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/CC/code_1120/func_80387584.s")
