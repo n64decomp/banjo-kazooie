9958f3a3d075eac123a819a6bd1e3ca48be7315f
Banjo Kazooie <banjo.decomp@gmail.com>
2021-12-01 12:00:43 -0600

    core2/rand.c done

--- src/core2/code_C3A40.c
+++ src/core2/code_C3A40.c
@@ -2,7 +2,13 @@
 #include "functions.h"
 #include "variables.h"
 
+extern f32 D_80379170;
 
 #pragma GLOBAL_ASM("asm/nonmatchings/core2/code_C3A40/func_8034A9D0.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/core2/code_C3A40/func_8034AA70.s")
+void func_8034AA70(f32 arg0[4]){
+    arg0[1] = D_80379170;
+    arg0[0] = 4000.0f;
+    arg0[3] = 2000.0f;
+    arg0[2] = -500.0f;
+}
