971ee85a3141a696778daf3db7c8d9b86e96a948
Banjo Kazooie <banjo.decomp@gmail.com>
2021-12-05 20:10:40 -0600

    core2/code_D2180.c progress

--- src/core2/code_D01C0.c
+++ src/core2/code_D01C0.c
@@ -7,7 +7,7 @@ void func_8035765C(Actor *this);
 
 /* .data */
 extern ActorInfo D_80372870 = { 
-    0x1B2, 0x29F, 0x482, 
+    0x1B2, ACTOR_29F_CLUCKER, ASSET_482_MODEL_CLUCKER, 
     0, NULL, 
     func_8035765C, NULL, func_803575B8,
     { 0x0, 0x0, 0x0, 0x0}, 0.0f, { 0x0, 0x0, 0x0, 0x0}
@@ -22,6 +22,12 @@ extern ActorInfo D_80372870 = {
 
 #pragma GLOBAL_ASM("asm/nonmatchings/core2/code_D01C0/func_8035758C.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/core2/code_D01C0/func_803575B8.s")
+// #pragma GLOBAL_ASM("asm/nonmatchings/core2/code_D01C0/func_803575B8.s")
+Actor *func_803575B8(ActorMarker *marker, Gfx **gfx, Mtx **mtx, Vtx **vtx){
+    Actor *this = marker_getActor(marker);
+    func_8033A45C(3, (this->state == 5) ? 0 : 1);
+    func_8033A45C(4, (this->state == 0 || this->state == 1 || this->state == 6)? 0 : 1);
+    return func_80325888(marker, gfx, mtx, vtx);
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/core2/code_D01C0/func_8035765C.s")
