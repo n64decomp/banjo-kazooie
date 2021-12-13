1829eff2f1a1c9cd0a7b679becd38ee45110e913
Banjo Kazooie <banjo.decomp@gmail.com>
2021-12-13 15:12:24 -0600

    core2/ch/badshad.c progress

--- src/core2/ch/badShad.c
+++ src/core2/ch/badShad.c
@@ -2,6 +2,8 @@
 #include "functions.h"
 #include "variables.h"
 
+extern void func_802C3D3C(void (*)(s32, s32), s32, s32);
+
 Actor *func_802D6F48(ActorMarker *this, Gfx **gdl, Mtx **mptr, Vtx **arg3);
 void func_802D6EA0(Actor *this);
 
@@ -12,6 +14,9 @@ extern ActorInfo D_80367A50 = {
     func_802D6EA0, func_80326224, func_802D6F48,
     { 0x0, 0x0, 0x0, 0x0}, 0.0f, { 0x0, 0x0, 0x0, 0x0}
 };
+
+/* .rodata */
+extern f32 D_80376D40;
 
 /* .bss */
 extern f32 D_8037DE10[3];
@@ -32,7 +37,25 @@ void func_802D6EA0(Actor *this){
     }
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/core2/ch/badShad/func_802D6F48.s")
+Actor *func_802D6F48(ActorMarker *marker, Gfx **gfx, Mtx **mtx, Vtx **vtx){
+    f32 sp44[3];
+    f32 sp40;
+    Actor *this = func_80325300(marker, sp44);
+    Actor *other;
+    if(this->despawn_flag) 
+        return this;
+    
+    if(this->unk104){
+        other = marker_getActor(this->unk104);
+    }
+
+    if(other)
+        func_8033A410(other->alpha_124_19);
+    sp40 = ml_map_f(this->unk28, 0.0f , 800.0f, 0.53f, D_80376D40)*this->unk1C[1];
+    func_8033A4CC(2);
+    func_803391A4(gfx, mtx, this->position, sp44, sp40, NULL, func_80330B1C(marker));
+    return this;
+}
 
 f32 func_802D7038(Actor *);
 #pragma GLOBAL_ASM("asm/nonmatchings/core2/ch/badShad/func_802D7038.s")
@@ -40,6 +63,7 @@ f32 func_802D7038(Actor *);
 #pragma GLOBAL_ASM("asm/nonmatchings/core2/ch/badShad/func_802D7124.s")
 
 #ifndef NONMATCHING
+void func_802D71A0(ActorMarker *marker, f32 arg1);
 #pragma GLOBAL_ASM("asm/nonmatchings/core2/ch/badShad/func_802D71A0.s")
 #else
 void func_802D71A0(ActorMarker *marker, f32 arg1){
@@ -73,5 +97,45 @@ void func_802D71A0(ActorMarker *marker, f32 arg1){
 #endif
 
 
+#ifndef NONMATCHING
 #pragma GLOBAL_ASM("asm/nonmatchings/core2/ch/badShad/func_802D729C.s")
+#else
+void func_802D729C(Actor *actor, f32 arg1){
+    Actor *sp1C;
+    if(actor->despawn_flag){
+        if(actor->unk104){
+            sp1C = marker_getActor(actor->unk104);
+            sp1C->unk104 = NULL;
+            marker_getActor(actor->unk104)->despawn_flag = TRUE;
+        }
+        else{
+            actor->unk104 = NULL;
+            return;
+        }
+    }
+    if(!actor->unk104){
+        func_802C3D3C(func_802D71A0, actor->marker, reinterpret_cast(s32, arg1));
+        return;
+    }
 
+    sp1C = marker_getActor(actor->unk104);
+    if(sp1C->despawn_flag == TRUE) 
+        return;
+
+    if(!((func_8023DB5C() ^ actor->marker->actrArrayIdx) & 0x7)){
+        sp1C->unk28 = func_802D7038(actor);
+        sp1C->position_x = D_8037DE10[0];
+        sp1C->position_y = D_8037DE10[1] + 6.0f;
+        sp1C->position_z = D_8037DE10[2];
+        sp1C->yaw = D_8037DE20[1];
+        sp1C->pitch = D_8037DE20[0];
+        sp1C->roll = D_8037DE20[2];
+    }
+    else{
+        sp1C->position_x = actor->position_x;
+        sp1C->position_z = actor->position_z;
+    }
+    sp1C->unk1C[0] = func_8023DB5C();
+    sp1C->unk1C[1] = arg1;
+}
+#endif
--- src/core2/code_D5D10.c
+++ src/core2/code_D5D10.c
@@ -2,14 +2,23 @@
 #include "functions.h"
 #include "variables.h"
 
-void func_8035CF1C(Actor *this);
+typedef struct {
+    u8 pad0[0x8];
+    u8 unk8;
+    u8 unk9;
+    u8 unkA;
+    u8 padB[0x1];
+    u8 unkC;
+}ActorLocal_Skeleton;
+
+void chskeleton_update(Actor *this);
 
 /* .data */
 extern ActorAnimationInfo D_80372D20[];
 extern ActorInfo D_80372D78 = { 
     0x218, 0x34E, 0x4CC, 
     0x1, D_80372D20, 
-    func_8035CF1C, func_80326224, func_80325888, 
+    chskeleton_update, func_80326224, func_80325888, 
     { 0x9, 0xC4, 0x0, 0x0}, 1.0f, { 0x0, 0x0, 0x0, 0x0}
 };
 
@@ -20,4 +29,9 @@ extern ActorInfo D_80372D78 = {
 
 #pragma GLOBAL_ASM("asm/nonmatchings/core2/code_D5D10/func_8035CE50.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/core2/code_D5D10/func_8035CF1C.s")
+void chskeleton_update(Actor *this){
+    if(!this->unk16C_4){
+        func_8035CE50(this);
+    }
+    func_802DB5A0(this);
+}
