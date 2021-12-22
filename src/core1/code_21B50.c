889256fac6f391fca9815aa6175523f8cd845b93
Banjo Kazooie <banjo.decomp@gmail.com>
2021-12-21 20:58:34 -0600

    39.9117%

--- src/core1/code_21B50.c
+++ src/core1/code_21B50.c
@@ -1,10 +1,47 @@
 #include <ultra64.h>
 #include "functions.h"
 #include "variables.h"
+#include "n_synth.h"
 
-
+#ifndef NONMATCHING
 #pragma GLOBAL_ASM("asm/nonmatchings/core1/code_21B50/func_8025F570.s")
+#else
+void func_8025F570(N_ALSeqPlayer *arg0, u8 arg1){
+    ALEvent evt;
 
+    evt.type = AL_SEQP_MIDI_EVT;
+    evt.msg.midi.ticks = 0;
+    evt.msg.midi.status = AL_MIDI_ChannelModeSelect;
+    evt.msg.midi.byte1 = 0x7F;
+    evt.msg.midi.byte2 = arg1;
+    alEvtqPostEvent(&arg0->evtq, &evt, 0);
+}
+#endif
+
+#ifndef NONMATCHING
 #pragma GLOBAL_ASM("asm/nonmatchings/core1/code_21B50/func_8025F5C0.s")
+#else
+void func_8025F5C0(N_ALSeqPlayer *arg0, u8 arg1){
+    ALEvent evt;
 
-#pragma GLOBAL_ASM("asm/nonmatchings/core1/code_21B50/func_8025F610.s")
+    evt.type = AL_SEQP_MIDI_EVT;
+    evt.msg.midi.ticks = 0;
+    evt.msg.midi.status = AL_MIDI_ChannelModeSelect;
+    evt.msg.midi.byte1 = 0x7E;
+    evt.msg.midi.byte2 = arg1;
+    alEvtqPostEvent(&arg0->evtq, &evt, 0);
+}
+#endif
+
+void func_8025F610(void){
+    n_syn->synth.head = NULL;
+    n_syn->synth.n_seqp1 = NULL;
+    n_syn->synth.n_seqp2 = NULL;
+    n_syn->unk5C = NULL;
+    n_syn->unk60 = NULL;
+    n_syn->unk64 = NULL;
+    n_syn->unk68 = NULL;
+    n_syn->unk6C = NULL;
+    n_syn->unk70 = NULL;
+    n_syn->synth.n_sndp = NULL;
+}
--- src/core1/code_25E20.c
+++ src/core1/code_25E20.c
@@ -3,14 +3,14 @@
 #include "variables.h"
 
 
-#pragma GLOBAL_ASM("asm/nonmatchings/core1/code_25E20/func_80263840.s")
+void func_80263840(void){}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/core1/code_25E20/func_80263848.s")
+void func_80263848(void){}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/core1/code_25E20/func_80263850.s")
 
 #pragma GLOBAL_ASM("asm/nonmatchings/core1/code_25E20/func_802639FC.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/core1/code_25E20/func_80263B1C.s")
+void func_80263B1C(void){}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/core1/code_25E20/func_80263B24.s")
+void func_80263B24(void){}
