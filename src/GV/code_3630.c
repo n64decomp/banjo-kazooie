326e5bedf89c94abc65b8f175741d41fccdea18a
Banjo Kazooie <banjo.decomp@gmail.com>
2021-12-23 11:44:59 -0600

    40.2816%

--- src/GV/code_3630.c
+++ src/GV/code_3630.c
@@ -5,8 +5,16 @@
 void func_80389B1C(Actor *this);
 
 /* .data */
-extern ActorAnimationInfo D_80390EA0[];
-extern ActorInfo D_80390ED0 = { 0xA9, 0x11B, 0x3DD, 
+ActorAnimationInfo D_80390EA0[] = {
+    {0x00, 0.0f},
+    {0xDF, 4.5f},
+    {0xE3, 2.0f},
+    {0xE4, 1.95f},
+    {0xCE, 2.4f},
+    {0xDF, 4.5f}
+};
+
+ActorInfo D_80390ED0 = { MARKER_A9_RUBEE, ACTOR_11B_RUBEE, ASSET_3DD_MODEL_RUBEE, 
     0x2, D_80390EA0,
     func_80389B1C, func_80326224, func_80325888,
     { 0x9, 0xC4, 0x0, 0x0}, 1.6f, { 0x0, 0x0, 0x0, 0x0}
--- src/GV/code_5570.c
+++ src/GV/code_5570.c
@@ -2,18 +2,38 @@
 #include "functions.h"
 #include "variables.h"
 
+extern void func_8025AE50(s32, f32);
+
+
 void func_8038BEA0(Actor *this);
 
 /* .data */
-extern ActorAnimationInfo D_80391120[];
-extern ActorInfo D_80391158 = { 0xA6, 0x118, 0x371, 
+ActorAnimationInfo D_80391120[] ={
+    {0x00, 0.0f},
+    {0xC5, 8000000.0f},
+    {0xC5, 1.6f},
+    {0xC7, 1.8f},
+    {0xC6, 0.55f},
+    {0xC8, 0.8f},
+    {0xC7, 1.8f}
+};
+
+ActorInfo D_80391158 = { 0xA6, 0x118, 0x371, 
     0x1, D_80391120, 
     func_8038BEA0, func_80326224, func_80325888, 
     { 0x0, 0x0, 0x0, 0x0}, 0.0f, { 0x0, 0x0, 0x0, 0x0}
 };
 
+s32 D_8039117C[3] = {160, 100, 0};
+
+
+/* .bss */
+extern s32 D_80391A80;
+
 /* .code */
-#pragma GLOBAL_ASM("asm/nonmatchings/GV/code_5570/func_8038B960.s")
+void func_8038B960(void){
+    func_8025AE50(5000, 3.19f);
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/GV/code_5570/func_8038B988.s")
 
@@ -33,4 +53,6 @@ extern ActorInfo D_80391158 = { 0xA6, 0x118, 0x371,
 
 #pragma GLOBAL_ASM("asm/nonmatchings/GV/code_5570/func_8038BEA0.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/GV/code_5570/func_8038C5BC.s")
+s32 func_8038C5BC(void){
+    return D_80391A80;
+}
