24f7c2cd576eaf83b13bea5cfcc690841955c6df
Banjo Kazooie <banjo.decomp@gmail.com>
2021-12-23 10:51:43 -0600

    GV/code_3AA0.c done, GV/code_3B10.c done

--- src/GV/code_3AA0.c
+++ src/GV/code_3AA0.c
@@ -6,17 +6,20 @@ void func_80389E90(Actor *this);
 void func_80389EA8(Actor *this);
 
 /* .data */
-extern ActorInfo D_80390F00 = { 0xAB, 0x11D, 0x3E1, 
+ActorInfo D_80390F00 = { 0xAB, 0x11D, 0x3E1, 
     0x1, NULL, 
     func_80389E90, func_80389EA8, func_80325888, 
     { 0x9, 0xC4, 0x0, 0x0}, 0.9f, { 0x0, 0x0, 0x0, 0x0}
 };
 
 /* .code */
-#pragma GLOBAL_ASM("asm/nonmatchings/GV/code_3AA0/func_80389E90.s")
+void func_80389E90(Actor *this){
+    this->marker->propPtr->unk8_3 = FALSE;
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/GV/code_3AA0/func_80389EA8.s")
+void func_80389EA8(Actor *this){
+    this->marker->propPtr->unk8_3 = FALSE;
+    this->unk4C = 100 + 40*func_8038E178();
+    func_80343DEC(this);
 
-#pragma GLOBAL_ASM("asm/nonmatchings/GV/code_3AA0/func_80389F00.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/GV/code_3AA0/func_80389F5C.s")
+}
