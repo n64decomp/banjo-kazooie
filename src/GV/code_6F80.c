735e9276ff25804ebaa7d57c68b28023fbaa49ca
Banjo Kazooie <banjo.decomp@gmail.com>
2021-12-24 03:33:48 -0600

    GV/code_6F80.c done

--- src/GV/code_6F80.c
+++ src/GV/code_6F80.c
@@ -5,15 +5,36 @@
 void func_8038D3AC(Actor *this);
 
 /* .data */
-extern ActorInfo D_80391260 = { 0x1A5, 0x288, 0x402, 
+ActorInfo D_80391260 = { 0x1A5, 0x288, 0x402, 
     0, NULL, 
     func_8038D3AC, NULL, func_80325340, 
     { 0x0, 0x0, 0x0, 0x0}, 0.0f, { 0x0, 0x0, 0x0, 0x0}
 };
 
+/* .bss */
+u8 D_80391AA0;
+
 /* .code */
-#pragma GLOBAL_ASM("asm/nonmatchings/GV/code_6F80/func_8038D370.s")
+void func_8038D370(ActorMarker *this_marker, ActorMarker *other_marker){
+    D_80391AA0 = 5;
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/GV/code_6F80/func_8038D388.s")
+int func_8038D388(void){
+    return D_80391AA0 ? 1 : 0;
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/GV/code_6F80/func_8038D3AC.s")
+void func_8038D3AC(Actor *this){
+    if(!this->unk16C_4){
+        this->unk16C_4 = TRUE;
+        func_803300A8(this->marker, func_8038D370, NULL, NULL);
+        D_80391AA0 = 0;
+        if(func_8031FF44(BKPROG_F8_KING_SANDYBUTT_PYRAMID_STATE, 2) == 3){
+            marker_despawn(this->marker);
+        }
+    }
+    else{
+        if(D_80391AA0 > 0){
+            D_80391AA0--;
+        }
+    }
+}
