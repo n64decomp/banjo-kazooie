9e9455d2b3722dfe1d3a825dfd244699803722e5
Banjo Kazooie <banjo.decomp@gmail.com>
2022-03-17 21:22:53 -0500

    CCW/code_7570.c done

--- src/CCW/code_7570.c
+++ src/CCW/code_7570.c
@@ -2,7 +2,39 @@
 #include "functions.h"
 #include "variables.h"
 
+void func_8038D9E0(Actor *this);
 
-#pragma GLOBAL_ASM("asm/nonmatchings/CCW/code_7570/func_8038D960.s")
+/* .data */
+extern ActorInfo D_8038F6F0 = { 0x1C3, 0x30C, 0x4E4, 0x0, NULL, func_8038D9E0, NULL, func_80325888, { 0x0, 0x0}, 0, 0.0f, { 0x0, 0x0, 0x0, 0x0}};
 
-#pragma GLOBAL_ASM("asm/nonmatchings/CCW/code_7570/func_8038D9E0.s")
+/* .code */
+void func_8038D960(Actor *this, s32 next_state) {
+    if (next_state == 1) {
+        func_80335924(this->unk148, 0x21C, 0.0f, 6.0f);
+        func_80335A8C(this->unk148, 2);
+    }
+    if (next_state == 2) {
+        func_80326310(this);
+    }
+    this->state = next_state;
+}
+
+void func_8038D9E0(Actor *this) {
+    f32 sp24;
+    f32 sp20;
+    ActorMarker *temp_v0;
+
+    if (!this->unk16C_4) {
+        this->unk16C_4 = TRUE;
+        this->unk124_9 = 2;
+        this->marker->unk40_22 = TRUE;
+        func_8038D960(this, 1);
+    }
+    if (this->state == 1) {
+        this = this;
+        func_8033568C(this->unk148, &sp24, &sp20);
+        if ((sp24 < 0.25f) && (sp20 >= 0.25f)) {
+            func_8038D960(this, 2);
+        }
+    }
+}
