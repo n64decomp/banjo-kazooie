3939979aa2d637dd75d1117ebc2eba66ad8a90eb
Banjo Kazooie <banjo.decomp@gmail.com>
2022-03-17 22:07:13 -0500

    CCW/code_6EC0.c done

--- src/CCW/code_6AC0.c
+++ src/CCW/code_6AC0.c
@@ -8,16 +8,3 @@
 #pragma GLOBAL_ASM("asm/nonmatchings/CCW/code_6AC0/func_8038CEB0.s")
 
 #pragma GLOBAL_ASM("asm/nonmatchings/CCW/code_6AC0/func_8038CFB4.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/CCW/code_6AC0/func_8038D2B0.s")
-
-
-void func_8038D30C(ActorMarker* marker, Gfx** gfx, Mtx** mtx, Vtx** vtx) {
-    if (levelSpecificFlags_get(0x25) == 0) {
-        func_80325340(marker, gfx, mtx, vtx);
-        return;
-    }
-    func_80325888(marker, gfx, mtx, vtx);
-}
-
-#pragma GLOBAL_ASM("asm/nonmatchings/CCW/code_6AC0/func_8038D368.s")
