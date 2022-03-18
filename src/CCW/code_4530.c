96379d670d4ce79537f5750bbb594e37e79b4839
Banjo Kazooie <banjo.decomp@gmail.com>
2022-03-18 18:38:16 -0500

    CCW/code_4960.c done

--- src/CCW/code_4530.c
+++ src/CCW/code_4530.c
@@ -11,9 +11,3 @@ void func_8038A920(ActorMarker* marker, s32 arg1, s32 arg2) {
 #pragma GLOBAL_ASM("asm/nonmatchings/CCW/code_4530/func_8038AA38.s")
 
 #pragma GLOBAL_ASM("asm/nonmatchings/CCW/code_4530/func_8038AA8C.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/CCW/code_4530/func_8038AD50.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/CCW/code_4530/func_8038AE64.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/CCW/code_4530/func_8038AEBC.s")
