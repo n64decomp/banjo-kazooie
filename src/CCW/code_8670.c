67b2f9c830bbac9221b005b11d3efa1afafca3bf
Banjo Kazooie <banjo.decomp@gmail.com>
2022-03-19 13:26:52 -0500

    CCW 100%

--- src/CCW/code_8670.c
+++ src/CCW/code_8670.c
@@ -2,7 +2,13 @@
 #include "functions.h"
 #include "variables.h"
 
-s32 func_8038EA60(s32 arg0, s32 arg1, s32 arg2) {
+void func_8038EAD4(Actor *this);
+
+/* .data */
+extern ActorInfo D_8038FA00 = { 0x1E3, 0x374, 0x0, 0x0, NULL, func_8038EAD4, func_80326224, func_80325340, { 0x0, 0x0}, 0, 0.0f, { 0x0, 0x0, 0x0, 0x0}};
+
+/* .code */
+bool func_8038EA60(s32 arg0, s32 arg1, s32 arg2) {
     s32 sp24;
     s32 temp_a0;
     s32 sp1C;
@@ -13,9 +19,9 @@ s32 func_8038EA60(s32 arg0, s32 arg1, s32 arg2) {
     temp_a0 = arg0 - sp1C;
     return ((temp_v1 * temp_v1) + (temp_a0 * temp_a0)) < (arg2 * arg2);
 }
-void func_8038EAD4(ActorMarker** marker) {
-    if (func_803203FC(0x13) != 0) {
-        marker_despawn(*marker);
+void func_8038EAD4(Actor* this) {
+    if (func_803203FC(0x13)) {
+        marker_despawn(this->marker);
         return;
     }
     if (func_8038EA60(0, -0x6B, 0xBC) != 0) {
