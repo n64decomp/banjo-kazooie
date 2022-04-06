989286e9852c0c60938a09b4cab9392137c49cb5
Banjo Kazooie <banjo.decomp@gmail.com>
2022-04-06 13:35:11 -0500

    core2/code_4BE10.c done

--- src/core2/code_4BE10.c
+++ src/core2/code_4BE10.c
@@ -12,6 +12,33 @@ extern ActorInfo D_803675F0 = {
     { 0x0, 0x0}, 0, 0.0f, { 0x0, 0x0, 0x0, 0x0}
 };
 
+extern struct31s D_80367624;
+extern struct42s D_8036764C;
+
 
 /* .code */
-#pragma GLOBAL_ASM("asm/nonmatchings/core2/code_4BE10/func_802D2DA0.s")
+void func_802D2DA0(Actor *this) {
+    ParticleEmitter *pCtrl;
+    f32 sp70[3];
+    s32 phi_s6;
+    s32 i;
+
+    if (this->state == 2) {
+        if (--this->unk38_31 == 0) {
+            marker_despawn(this->marker);
+        }
+        pCtrl = partEmitList_pushNew((s32) this->unk60);
+        for(phi_s6 = 0; phi_s6 < (s32)this->unk60; phi_s6++){
+            for(i = 0; i < 3; i++){
+                sp70[i] = this->position[i] + randf()*2*this->unkF4_8 - ((i == 1)? 0: this->unkF4_8);
+            }
+
+            particleEmitter_setSprite(pCtrl, ASSET_710_SPRITE_SPARKLE_PURPLE);
+            particleEmitter_setStartingFrameRange(pCtrl, 0, 0);
+            particleEmitter_setPosition(pCtrl, sp70);
+            particleEmitter_setPositionAndVelocityRanges(pCtrl, &D_8036764C);
+            func_802EFB98(pCtrl, &D_80367624);
+            particleEmitter_setSpawnInterval(pCtrl, 1.0f);
+        }
+    }
+}
