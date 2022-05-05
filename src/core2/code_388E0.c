c06245487cd5a3c7c9a9d57265d66b1b7f783541
Banjo Kazooie <banjo.decomp@gmail.com>
2022-05-05 16:03:03 -0500

    core2/code_388E0.c done

--- src/core2/code_388E0.c
+++ src/core2/code_388E0.c
@@ -2,111 +2,75 @@
 #include "functions.h"
 #include "variables.h"
 
-extern f32 func_80259198(f32, f32);
-extern f32 func_8028EBA4(void);
-extern f32 func_8028EBE4(void);
-extern void func_802BD870(f32, f32, f32, f32);
+extern f32 func_802BD51C(void);
 
-void func_802BFE50(f32 arg0, f32 arg1, f32 arg2);
-void func_802BFE74(bool);
-
-/* .rodata */
-extern f64 D_80375F10;
-extern f64 D_80375F18;
 
 /* .bss */
-extern f32 D_8037DB10;
-extern f32 D_8037DB14;
-extern f32 D_8037DB18;
-extern f32 D_8037DB1C;
-extern f32 D_8037DB20;
+f32 D_8037DAF0[3];
+f32 D_8037DB00[3];
+u8 D_8037DB0C;
+u8 D_8037DB0D;
+u8 D_8037DB0E;
 
 /* .code */
-#pragma GLOBAL_ASM("asm/nonmatchings/core2/code_388E0/func_802BF870.s")
+void func_802BF870(void){
+    D_8037DB0E = TRUE;
+}
 
 void func_802BF880(void){}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/core2/code_388E0/func_802BF888.s")
+void func_802BF888(void) {
+    f32 sp5C[3];
+    f32 sp50[3];
+    f32 sp44[3];
+    f32 sp38[3];
+    f32 sp2C[3];
+    f32 sp20[3];
+    f32 pad1C;
 
-#pragma GLOBAL_ASM("asm/nonmatchings/core2/code_388E0/func_802BF9B8.s")
+    if(D_8037DB0E)
+        return;
 
-void func_802BFA60(void) {
-    func_802BD870(10.0f, 20.0f, 120.0f, 200.0f);
-    if (map_get() == MAP_90_GL_BATTLEMENTS) {
-        func_802BFE50(10.0f, 800.0f, 1350.0f);
+    func_802BD384(sp44);
+    sp20[0] = D_8037DB00[0];
+    sp20[1] = D_8037DB00[1];
+    sp20[2] = D_8037DB00[2];
+    sp44[0] = D_8037DAF0[0];
+    sp44[2] = D_8037DAF0[2];
+    if (D_8037DB0C) {
+        sp44[1] = D_8037DAF0[1];
     } else {
-        func_802BFE50(2.0f, 800.0f, 350.0f);
+        sp44[1] = func_802BD51C();
     }
-    func_802BFE74(0);
+    func_802BE190(sp44);
+    if (D_8037DB0D) {
+        func_802BE60C();
+    }
+    func_802BD384(sp44);
+    func_802BD4C0(sp5C);
+    ml_vec3f_diff_copy(sp50, sp5C, sp44);
+    func_802BC434(sp38, sp5C, sp44);
+    func_802BC434(sp2C, sp5C, sp20);
+    if (gu_sqrtf(sp50[0]*sp50[0] + sp50[2]*sp50[2]) < 100.0f) {
+        sp38[1] = sp2C[1];
+    }
+    func_802BD904(sp38);
 }
 
-void func_802BFAE8(void){}
 
-void func_802BFAF0(void) {
-    f32 sp84[3];
-    f32 sp78[3];
-    f32 sp6C[3];
-    f32 sp60[3];
-    f32 sp54[3];
-    f32 sp48[3];
-    f32 sp44;
-    f32 temp_f10;
-    f32 sp3C;
-    f32 sp38;
-    f32 sp34;
+void func_802BF9B8(s32 arg0) {
+    f32 sp2C;
+    f32 sp28;
+    s32 temp_s0;
 
-    func_802BD384(sp6C);
-    func_802BD4C0(sp84);
-    sp84[1] += 40.0f;
-    sp34 = func_8028EBE4();
-    if (sp34 > 180.0f) {
-        sp3C = ml_map_f(sp34, 300.0f, 360.0f, 900.0f, D_8037DB18);
-        sp84[1] += ml_map_f(sp34, 300.0f, 360.0f, -140.0f, 70.0f);
-    } else {
-        sp3C = D_8037DB18;
-        sp84[1] += 70.0f;
-    }
-    sp38 = time_getDelta();
-    ml_vec3f_diff_copy(sp48, sp6C, sp84);
-    sp44 = gu_sqrtf(sp48[0]*sp48[0] + sp48[1]*sp48[1] +  sp48[2]*sp48[2]);
-    temp_f10 = (sp3C - sp44) * sp38;
-    sp44 += func_80259198(temp_f10 * D_8037DB10, sp38 * D_8037DB14);
-    func_8025727C(sp84[0], sp84[1], sp84[2], sp6C[0], sp6C[1], sp6C[2], &sp54[0], &sp54[1]);
-    if ((sp34 > 180.0f) && (sp34 < 360.0f)) {
-        sp34 = min_f(100.0f, (f32) ((f64) (360.0f - sp34) * D_80375F10));
-    }
-    sp48[0] = mlDiffDegF(mlNormalizeAngle(sp34), sp54[0]);
-    sp48[1] = mlDiffDegF(mlNormalizeAngle(func_8028EBA4() + 180.0f), sp54[1]);
-    sp48[2] = 0.0f;
-    sp48[0] = (f32) ((f64) sp48[0] * ((f64) sp38 * D_80375F18));
-    sp48[1] = sp48[1] * (sp38 * D_8037DB1C);
-    sp48[0] = func_80259198(sp48[0], sp38 * 40.0f);
-    sp48[1] = func_80259198(sp48[1], sp38 * D_8037DB20);
-    sp54[0] = mlNormalizeAngle(sp54[0] + sp48[0]);
-    sp54[1] = mlNormalizeAngle(sp54[1] + sp48[1]);
-
-    func_80256E24(sp60, -sp54[0], sp54[1], 0.0f, 0.0f, sp44);
-    ml_vec3f_add(sp78, sp84, sp60);
-    func_802BD334(sp78);
-    func_8025727C(sp84[0], sp84[1], sp84[2], sp78[0], sp78[1], sp78[2], &sp54[0], &sp54[1]);
-    sp54[0] = -sp54[0];
-    sp54[2] = 0.0f;
-    func_802BD720(sp54);
-}
-
-void func_802BFE50(f32 arg0, f32 arg1, f32 arg2){
-    D_8037DB10 = arg0;
-    D_8037DB14 = arg1;
-    D_8037DB18 = arg2;
-}
-
-void func_802BFE74(bool arg0) {
-    if (arg0) {
-        D_8037DB1C = 6.0f;
-        D_8037DB20 = 270.0f;
-    }
-    else{
-        D_8037DB1C = 2.0f;
-        D_8037DB20 = 90.0f;
-    }
+    temp_s0 = func_802B9E5C(arg0);
+    D_8037DB0C = func_802BA8BC(temp_s0);
+    D_8037DB0D = func_802BA87C(temp_s0);
+    func_802BA7D8(temp_s0, &D_8037DB00);
+    func_802BA78C(temp_s0, &D_8037DAF0);
+    func_802BA82C(temp_s0, &sp2C, &sp28);
+    func_802BE230(sp2C, sp28);
+    func_802BA854(temp_s0, &sp2C, &sp28);
+    func_802BE244(sp2C, sp28);
+    D_8037DB0E = 0;
 }
---
+++ src/core2/code_38AD0.c
@@ -0,0 +1,104 @@
+#include <ultra64.h>
+#include "functions.h"
+#include "variables.h"
+
+extern f32 func_80259198(f32, f32);
+extern f32 func_8028EBA4(void);
+extern f32 func_8028EBE4(void);
+extern void func_802BD870(f32, f32, f32, f32);
+
+void func_802BFE50(f32 arg0, f32 arg1, f32 arg2);
+void func_802BFE74(bool);
+
+/* .rodata */
+extern f64 D_80375F10;
+extern f64 D_80375F18;
+
+/* .bss */
+f32 D_8037DB10;
+f32 D_8037DB14;
+f32 D_8037DB18;
+f32 D_8037DB1C;
+f32 D_8037DB20;
+
+/* .code */
+void func_802BFA60(void) {
+    func_802BD870(10.0f, 20.0f, 120.0f, 200.0f);
+    if (map_get() == MAP_90_GL_BATTLEMENTS) {
+        func_802BFE50(10.0f, 800.0f, 1350.0f);
+    } else {
+        func_802BFE50(2.0f, 800.0f, 350.0f);
+    }
+    func_802BFE74(0);
+}
+
+void func_802BFAE8(void){}
+
+void func_802BFAF0(void) {
+    f32 sp84[3];
+    f32 sp78[3];
+    f32 sp6C[3];
+    f32 sp60[3];
+    f32 sp54[3];
+    f32 sp48[3];
+    f32 sp44;
+    f32 temp_f10;
+    f32 sp3C;
+    f32 sp38;
+    f32 sp34;
+
+    func_802BD384(sp6C);
+    func_802BD4C0(sp84);
+    sp84[1] += 40.0f;
+    sp34 = func_8028EBE4();
+    if (sp34 > 180.0f) {
+        sp3C = ml_map_f(sp34, 300.0f, 360.0f, 900.0f, D_8037DB18);
+        sp84[1] += ml_map_f(sp34, 300.0f, 360.0f, -140.0f, 70.0f);
+    } else {
+        sp3C = D_8037DB18;
+        sp84[1] += 70.0f;
+    }
+    sp38 = time_getDelta();
+    ml_vec3f_diff_copy(sp48, sp6C, sp84);
+    sp44 = gu_sqrtf(sp48[0]*sp48[0] + sp48[1]*sp48[1] +  sp48[2]*sp48[2]);
+    temp_f10 = (sp3C - sp44) * sp38;
+    sp44 += func_80259198(temp_f10 * D_8037DB10, sp38 * D_8037DB14);
+    func_8025727C(sp84[0], sp84[1], sp84[2], sp6C[0], sp6C[1], sp6C[2], &sp54[0], &sp54[1]);
+    if ((sp34 > 180.0f) && (sp34 < 360.0f)) {
+        sp34 = min_f(100.0f, (f32) ((f64) (360.0f - sp34) * D_80375F10));
+    }
+    sp48[0] = mlDiffDegF(mlNormalizeAngle(sp34), sp54[0]);
+    sp48[1] = mlDiffDegF(mlNormalizeAngle(func_8028EBA4() + 180.0f), sp54[1]);
+    sp48[2] = 0.0f;
+    sp48[0] = (f32) ((f64) sp48[0] * ((f64) sp38 * D_80375F18));
+    sp48[1] = sp48[1] * (sp38 * D_8037DB1C);
+    sp48[0] = func_80259198(sp48[0], sp38 * 40.0f);
+    sp48[1] = func_80259198(sp48[1], sp38 * D_8037DB20);
+    sp54[0] = mlNormalizeAngle(sp54[0] + sp48[0]);
+    sp54[1] = mlNormalizeAngle(sp54[1] + sp48[1]);
+
+    func_80256E24(sp60, -sp54[0], sp54[1], 0.0f, 0.0f, sp44);
+    ml_vec3f_add(sp78, sp84, sp60);
+    func_802BD334(sp78);
+    func_8025727C(sp84[0], sp84[1], sp84[2], sp78[0], sp78[1], sp78[2], &sp54[0], &sp54[1]);
+    sp54[0] = -sp54[0];
+    sp54[2] = 0.0f;
+    func_802BD720(sp54);
+}
+
+void func_802BFE50(f32 arg0, f32 arg1, f32 arg2){
+    D_8037DB10 = arg0;
+    D_8037DB14 = arg1;
+    D_8037DB18 = arg2;
+}
+
+void func_802BFE74(bool arg0) {
+    if (arg0) {
+        D_8037DB1C = 6.0f;
+        D_8037DB20 = 270.0f;
+    }
+    else{
+        D_8037DB1C = 2.0f;
+        D_8037DB20 = 90.0f;
+    }
+}
