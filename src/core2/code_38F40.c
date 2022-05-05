574ca24980466f6da910dc7dd34e9cc0c6886817
Banjo Kazooie <banjo.decomp@gmail.com>
2022-05-05 16:49:37 -0500

    code_396B0.c done

--- src/core2/code_38F40.c
+++ src/core2/code_38F40.c
@@ -5,7 +5,11 @@
 extern f32 D_80375F20;
 
 /*.bss */
-extern f32 D_8037DB30[3];
+extern struct {
+    f32 unk0;
+    f32 unk4;
+    f32 unk8;
+}D_8037DB30;
 
 /* .code */
 void func_802BFED0(void){
@@ -16,35 +20,34 @@ void func_802BFED0(void){
     player_getPosition(sp28);
     func_802BD384(sp1C);
     func_80257F18(sp28, sp1C, &sp34);
-    D_8037DB30[2] = sp34;
-    D_8037DB30[1] = 0.0f;
+    D_8037DB30.unk8 = sp34;
+    D_8037DB30.unk4 = 0.0f;
 }
 
 void func_802BFF1C(void) {
     func_802BE244(4.0f, 16.0f);
     func_802BE230(D_80375F20, 14.0f);
-    D_8037DB30[0] = 290.0f;
+    D_8037DB30.unk0 = 290.0f;
     func_802BFED0();
 }
 
 void func_802BFF78(void){}
 
 void func_802BFF80(f32 arg0){
-    D_8037DB30[0] = arg0;
+    D_8037DB30.unk0 = arg0;
 }
 
-#ifndef NONMATCHING
-#pragma GLOBAL_ASM("asm/nonmatchings/core2/code_38F40/func_802BFF8C.s")
-#else
 void func_802BFF8C(void) {
     f32 sp54[3];
     f32 sp48[3];
     f32 sp3C[3];
     f32 sp30[3];
+    f32 tmp1;
 
     player_getPosition(sp3C);
     sp3C[1] += 160.0f;
-    func_80256E24(sp30, D_8037DB30[1], D_8037DB30[2], 0.0f, 0.0f, D_8037DB30[0]);
+    tmp1 = D_8037DB30.unk8;
+    func_80256E24(sp30, D_8037DB30.unk4, tmp1, 0.0f, 0.0f, D_8037DB30.unk0);
     sp3C[0] += sp30[0];
     sp3C[1] += sp30[1];
     sp3C[2] += sp30[2];
@@ -55,7 +58,7 @@ void func_802BFF8C(void) {
         case MAP_27_FP_FREEZEEZY_PEAK:
         case MAP_48_FP_MUMBOS_SKULL:
             if (!func_802BE834(sp3C)) {
-                D_8037DB30[1] = max_f(D_8037DB30[1] - 8.0f, -75.0f);
+                D_8037DB30.unk4 = max_f(D_8037DB30.unk4 - 8.0f, -75.0f);
             }
             break;
         default:
@@ -72,4 +75,3 @@ void func_802BFF8C(void) {
     sp48[0] = mlNormalizeAngle(-sp48[0]);
     func_802BD904(sp48);
 }
-#endif
