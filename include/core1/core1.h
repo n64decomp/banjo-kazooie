52c52844df291af20454f827f08c40513b7a8e06
BraydonKains <kainsbraydon@gmail.com>
2022-05-13 23:43:46 -0400

    successfully matched func_80387A40

--- include/core1/core1.h
+++ include/core1/core1.h
@@ -3,6 +3,9 @@
 
 #include "core1/mem.h"
 
+void func_8025235C(f32*, f32*);
+void func_80252C08(f32[3], f32[3], f32, f32[3]);
 
+void func_80252C08(f32 arg0[3], f32 arg1[3], f32 scale, f32 arg3[3]);
 
 #endif
