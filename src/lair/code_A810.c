92b04336ad1bfbb772923811b0c214e1abba02db
Banjo Kazooie <banjo.decomp@gmail.com>
2022-05-22 12:53:49 -0500

    lair/code_A810.c

--- src/lair/code_A810.c
+++ src/lair/code_A810.c
@@ -2,20 +2,23 @@
 #include "functions.h"
 #include "variables.h"
 
+extern func_80391EA8(ActorMarker *, ActorMarker *, f32 [3], f32, f32, s32);
+
+typedef struct {
+    f32 unk0[2][3];
+}ActorLocal_lair_A810;
+
+
 void func_80390E28(Actor *this);
 
 /* .data */
-extern ActorInfo D_80394A80 = { 0x1EC, 0x377, 0x0, 0x0, NULL, func_80390E28, func_80326224, func_80325340, { 0xB, 0xB8}, 0, 0.0f, { 0x0, 0x0, 0x0, 0x0}};
+ActorInfo D_80394A80 = { 0x1EC, 0x377, 0x0, 0x0, NULL, func_80390E28, func_80326224, func_80325340, { 0xB, 0xB8}, 0, 0.0f, { 0x0, 0x0, 0x0, 0x0}};
 
-/* .rodata */
-extern f32 D_803952B0; //! 0.2f
-extern f32 D_803952B4; //! 0.6f
-extern f32 D_803952B8;
+/* .bss */
+extern f32 D_80395370[14][3];
+extern s32 D_80395460;
 
 /* .code */
-void func_80391EA8(ActorMarker *, ActorMarker *, f32 [3], f32, f32, s32);
-
-
 void func_80390C00(ActorMarker *marker)
 {
     Actor *actor;
@@ -28,7 +31,7 @@ void func_80390C00(ActorMarker *marker)
     func_80391EA8(
         newActor->marker, actor->marker, actor->unk1C,
         randf2(1400, 1800),
-        randf2(D_803952B0, D_803952B4),
+        randf2(0.2f, 0.6f),
         randf() < 0.5 ? 1 : 0
     );
 }
@@ -45,29 +48,28 @@ void func_80390CB4(ActorMarker *marker)
     func_80391EA8(
         newActor->marker, actor->marker, actor->unk1C,
         randf2(1200, 1400),
-        randf2(D_803952B8, 1),
+        randf2(0.8f, 1),
         -1
     );
 }
 
 void func_80390D3C(Actor *this, s32 val)
 {
+    ActorLocal_lair_A810 *local;
+
+    local = (ActorLocal_lair_A810 *)&this->local;
     func_80328A84(this, val);
 
     switch (val)
     {
         case 2:
         {
-            if (this->unk10_12)
+            if (this->unk10_12 != 0)
             {
                 s32 rand = randi2(0, this->unk10_12);
-
-                f32 *ptr = (f32 *)&this->unk7C[rand * 0xC];
-
-                //! Using f32 array access doesn't match?
-                this->unk1C[0] = *(f32 *)((u32)ptr + 0);
-                this->unk1C[1] = *(f32 *)((u32)ptr + 4);
-                this->unk1C[2] = *(f32 *)((u32)ptr + 8);
+                this->unk1C[0] = local->unk0[rand][0];
+                this->unk1C[1] = local->unk0[rand][1];
+                this->unk1C[2] = local->unk0[rand][2];
             }
             else
             {
@@ -76,7 +78,7 @@ void func_80390D3C(Actor *this, s32 val)
                 this->unk1C[2] = this->position_z;
             }
 
-            func_802C3C88((void *)func_80390C00, this->marker);
+            func_802C3C88(func_80390C00, this->marker);
 
             break;
         }
@@ -86,7 +88,7 @@ void func_80390D3C(Actor *this, s32 val)
             this->unk1C[1] = this->position_y;
             this->unk1C[2] = this->position_z;
 
-            func_802C3C88((void *)func_80390CB4, this->marker);
+            func_802C3C88(func_80390CB4, this->marker);
 
             break;
         }
@@ -95,4 +97,52 @@ void func_80390D3C(Actor *this, s32 val)
     }
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/lair/code_A810/func_80390E28.s")
+void func_80390E28(Actor *this) {
+    ActorLocal_lair_A810 *local;
+    bool sp38;
+    s32 i;
+    f32 temp_f0;
+
+    local = (ActorLocal_lair_A810 *)&this->local;
+    sp38 = mapSpecificFlags_getN(0, 2);
+    if (!this->initialized) {
+        if (D_80395460 == 0) {
+            D_80395460 = func_8030508C(0x377, D_80395370[0], 0x14);
+        }
+        this->unk10_12 = 0;
+        for(i = 0; (i < D_80395460) && (this->unk10_12 < 4); i++){
+            temp_f0 = func_80256064(this->position, D_80395370[i]);
+            if ((temp_f0 > 400.0f) && (temp_f0 < 1200.0f)) {
+                local->unk0[this->unk10_12][0] = D_80395370[i][0];
+                local->unk0[this->unk10_12][1] = D_80395370[i][1];
+                local->unk0[this->unk10_12][2] = D_80395370[i][2];
+                this->unk10_12++;
+            }
+        }
+        this->initialized = TRUE;
+    }
+    if (!this->unk16C_4) {
+        if (sp38 != 0) {
+            mapSpecificFlags_setN(0, 0, 2);
+        }
+        this->unk16C_4 = TRUE;
+        return;
+    }
+
+    switch (this->state) {
+        case 0:
+            if( !func_8031A3BC() && (sp38 < 2) && (randf() < 0.1) && !mapSpecificFlags_get(0xA) && !func_803203FC(0x1F)) {
+                sp38++;
+                mapSpecificFlags_setN(0, sp38, 2);
+                func_80390D3C(this, (randf() < 0.5) ? 2 :1);
+            }
+            break;
+        case 1:
+        case 2:
+            if (this->unk38_0) {
+                func_80390D3C(this, 0);
+                this->unk38_0 = FALSE;
+            }
+            break;
+    }
+}
