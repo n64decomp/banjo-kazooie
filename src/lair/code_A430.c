5ec59e49d1ea8cd6c7f2aab14fe95c4a7e7df44a
Banjo Kazooie <banjo.decomp@gmail.com>
2022-05-22 13:28:30 -0500

    lair/code_A430.c done

--- src/lair/code_A430.c
+++ src/lair/code_A430.c
@@ -5,7 +5,7 @@
 void func_80390820(Actor *this);
 
 /* .data */
-extern ActorInfo D_80394980 = { 0x299, 0x3C3, 0x0, 0x0, NULL, func_80390820, func_80326224, func_80325340, { 0x0, 0x0}, 0, 0.0f, { 0x0, 0x0, 0x0, 0x0}};
+ActorInfo D_80394980 = { 0x299, 0x3C3, 0x0, 0x0, NULL, func_80390820, func_80326224, func_80325340, { 0x0, 0x0}, 0, 0.0f, { 0x0, 0x0, 0x0, 0x0}};
 
 /* .code */
 void func_80390820(Actor *this)
