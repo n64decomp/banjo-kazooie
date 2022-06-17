4bb3d5fa1932d36eb2038b862b41ff8efc330452
Ryan Myers <ryan.p.myers@gmail.com>
2022-06-17 11:31:12 -0400

    Match func_8035489C

--- src/core2/code_CD3F0.c
+++ src/core2/code_CD3F0.c
@@ -6,7 +6,7 @@
 
 extern u8 D_803726D4[];
 
-void func_80354380(s32 arg0, f32 arg1) {
+void func_80354380(f32 *arg0, f32 arg1) {
     u8 sp1F;
     u8 sp1E;
     ParticleStruct0s* sp18;
@@ -34,7 +34,33 @@ void func_80354670(u8 arg0, s32 arg1) {
 
 #pragma GLOBAL_ASM("asm/nonmatchings/core2/code_CD3F0/func_803546E8.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/core2/code_CD3F0/func_8035489C.s")
+void func_8035489C(void) {
+    ParticleStruct0s* temp_s0;
+    u8 temp_v0;
+    f32 playerVelocity[3];
+    f32 playerPosition[3];
+
+    temp_s0 = func_8033E960();
+    temp_v0 = func_8033E8D0();
+    func_8033FE2C(temp_v0, 7.0f);
+    temp_s0->unk20--;
+    if (temp_s0->unk20 < 0) {
+        func_8033E984();
+        return;
+    }
+    func_80354670(temp_v0, temp_s0->unk20);
+    if (temp_s0->unk20 >= 0x10) {
+        player_getPosition(playerPosition);
+        player_getVelocity(playerVelocity);
+        playerVelocity[1] = 0.0f;
+        playerPosition[0] += playerVelocity[0] * time_getDelta();
+        playerPosition[2] += playerVelocity[2] * time_getDelta();
+        playerPosition[0] += temp_s0->unk0;
+        playerPosition[1] += temp_s0->unk4;
+        playerPosition[2] += temp_s0->unk8;
+        func_8033FEC8(temp_v0, playerPosition);
+    }
+}
 
 void func_80354990(void){}
 
