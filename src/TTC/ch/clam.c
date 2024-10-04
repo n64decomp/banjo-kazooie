581b467d2f0a0a4dc4e129cdbf476bbc8279c333
Bl00D4NGEL <git@d-peters.com>
2024-10-04 17:48:02 +0200

    refac: make clam functions static where applicable

--- src/TTC/ch/clam.c
+++ src/TTC/ch/clam.c
@@ -6,11 +6,11 @@ extern void spawnQueue_bundle_f32(s32, s32, s32, s32);
 extern f32 func_80257204(f32, f32, f32, f32);
 extern ActorProp * func_80320EB0(ActorMarker *, f32, s32);
 
-void chClam_updateFunc(Actor *this);
+static void __chClam_updateFunc(Actor *this);
 
 /* .data */
 ActorAnimationInfo gChClamAnimations[4] = {
-    {0x00, 0.0f},
+    {NULL, NULL},
     {ASSET_AA_ANIM_CLAM_IDLE, 2.0f},
     {ASSET_24_ANIM_CLAM_HOP,  1.0f},
     {ASSET_AB_ANIM_CLAM_EAT,  0.6f}
@@ -19,12 +19,12 @@ ActorAnimationInfo gChClamAnimations[4] = {
 ActorInfo gChClam = { 
     MARKER_15_CLAM, ACTOR_69_CLAM, ASSET_351_MODEL_CLAM,
     0x1, gChClamAnimations,
-    chClam_updateFunc, func_80326224, actor_draw, 
+    __chClam_updateFunc, func_80326224, actor_draw, 
     4500, 0x366, 1.6f, 0
 };
 
 /* .code */
-void chClam_playSfx(enum sfx_e sfx_id, f32 volume, s32 sampleRate, f32 position[3], f32 minFadeDistance, f32 maxFadeDistance){
+static void __chClam_playSfx(enum sfx_e sfx_id, f32 volume, s32 sampleRate, f32 position[3], f32 minFadeDistance, f32 maxFadeDistance){
     if(func_803114B0()){
         sampleRate -= 10000;
         if(sampleRate < 0)
@@ -34,13 +34,13 @@ void chClam_playSfx(enum sfx_e sfx_id, f32 volume, s32 sampleRate, f32 position[
     func_8030E878(sfx_id, volume, sampleRate, position, minFadeDistance, maxFadeDistance);
 }
 
-void chClam_func_80386454(Actor *this){
+static void __chClam_func_80386454(Actor *this){
     subaddie_set_state_with_direction(this, 1, 0.01f, 1);
     actor_loopAnimation(this);
     animctrl_setDuration(this->animctrl, randf2(1.9f, 2.1f));
 }
 
-bool chClam_updateFuncTarget(Actor *this, f32 arg1) {
+static bool __chClam_updateFuncTarget(Actor *this, f32 arg1) {
     f32 egg_dist;
     f32 red_feather_dist;
     f32 pad;
@@ -84,13 +84,13 @@ bool chClam_updateFuncTarget(Actor *this, f32 arg1) {
     if ((volatileFlag_get(VOLATILE_FLAG_C1_IN_FINAL_CHARACTER_PARADE) ? 0 : 0x11) < this->actor_specific_1_f) {
         this->actor_specific_1_f = (volatileFlag_get(VOLATILE_FLAG_C1_IN_FINAL_CHARACTER_PARADE) != 0) ? 0.0f : 17.0f;
     } else if (sp38 == 0) {
-        chClam_playSfx(SFX_AE_YUMYUM_TALKING, randf2(0.9f, 1.0f), 22000, this->position, 500.0f, 2000.0f);
+        __chClam_playSfx(SFX_AE_YUMYUM_TALKING, randf2(0.9f, 1.0f), 22000, this->position, 500.0f, 2000.0f);
     }
     return TRUE;
 
 }
 
-bool chClam_rotateTowardTarget(Actor *this, s32 arg1) {
+static bool __chClam_rotateTowardTarget(Actor *this, s32 arg1) {
     f32 temp_f0_2;
     s32 position;
     s32 sp2C;
@@ -100,7 +100,7 @@ bool chClam_rotateTowardTarget(Actor *this, s32 arg1) {
 
     animctrl_setDuration(this->animctrl, 1.0f);
     sp2C = (s32) ((f64) (60.0f / (f32) func_8033DD90()) * 0.5);
-    if ((this->unk1C[0] != 0.0f) || !chClam_updateFuncTarget(this, sp2C)) {
+    if ((this->unk1C[0] != 0.0f) || !__chClam_updateFuncTarget(this, sp2C)) {
         if (((f64) animctrl_getAnimTimer(this->animctrl) < 0.1) && ((f64) randf() < 0.5)) {
             if (this->unk1C[0] != 0.0f) {
                 arg1 *= 2;
@@ -132,7 +132,7 @@ bool chClam_rotateTowardTarget(Actor *this, s32 arg1) {
 }
 
 
-void chClam_particalEmitterInit(ParticleEmitter *pCtrl, f32 position[3]){
+static void __chClam_particalEmitterInit(ParticleEmitter *pCtrl, f32 position[3]){
     particleEmitter_setPosition(pCtrl, position);
     func_802EF9F8(pCtrl, 0.7f);
     func_802EFA18(pCtrl, 3);
@@ -144,7 +144,7 @@ void chClam_particalEmitterInit(ParticleEmitter *pCtrl, f32 position[3]){
     particleEmitter_setDrawMode(pCtrl, 2);
 }
 
-void chClam_emitLargeShellParticles(f32 position[3], s32 count){
+static void __chClam_emitLargeShellParticles(f32 position[3], s32 count){
     static struct41s D_8038C3F4 = {
         {{-50.0f,   750.0f, -50.0f}, {120.0f,   900.0f, 120.0f}},
         {{  0.0f, -1800.0f,   0.0f}, {  0.0f, -1800.0f,   0.0f}}
@@ -152,7 +152,7 @@ void chClam_emitLargeShellParticles(f32 position[3], s32 count){
     ParticleEmitter *pCtrl;
 
     pCtrl = partEmitMgr_newEmitter(count);
-    chClam_particalEmitterInit(pCtrl, position);
+    __chClam_particalEmitterInit(pCtrl, position);
     particleEmitter_setModel(pCtrl, ASSET_37C_MODEL_CLAM_LARGE_SHELL_PIECE);
     particleEmitter_setVelocityAndAccelerationRanges(pCtrl, &D_8038C3F4);
     particleEmitter_setAngularVelocityRange(pCtrl, -600.0f, -600.0f, -600.0f, 600.0f, 600.0f, 600.0f);
@@ -160,7 +160,7 @@ void chClam_emitLargeShellParticles(f32 position[3], s32 count){
     particleEmitter_emitN(pCtrl, count);
 }
 
-void chClam_emitEyeParticles(f32 position[3], s32 count){
+static void __chClam_emitEyeParticles(f32 position[3], s32 count){
     static struct41s D_8038C424 = {
         {{-80.0f,   400.0f, -80.0f}, {160.0f,   860.0f, 160.0f}},
         {{  0.0f, -1400.0f,   0.0f}, {  0.0f, -1400.0f,   0.0f}}
@@ -168,7 +168,7 @@ void chClam_emitEyeParticles(f32 position[3], s32 count){
     ParticleEmitter *pCtrl;
 
     pCtrl = partEmitMgr_newEmitter(count);
-    chClam_particalEmitterInit(pCtrl, position);
+    __chClam_particalEmitterInit(pCtrl, position);
     particleEmitter_setModel(pCtrl, ASSET_37D_MODEL_CLAM_EYE);
     particleEmitter_setVelocityAndAccelerationRanges(pCtrl, &D_8038C424);
     particleEmitter_setAngularVelocityRange(pCtrl, -300.0f, -300.0f, -300.0f, 300.0f, 300.0f, 300.0f);
@@ -176,7 +176,7 @@ void chClam_emitEyeParticles(f32 position[3], s32 count){
     particleEmitter_emitN(pCtrl, count);
 }
 
-void chClam_emitSmallShellParticles(f32 position[3], s32 count){
+static void __chClam_emitSmallShellParticles(f32 position[3], s32 count){
     static struct41s D_8038C454 = {
         {{-200.0f,  850.0f, -200.0f}, {400.0f,  1000.0f, 400.0f}},
         {{  0.0f, -1800.0f,    0.0f}, {  0.0f, -1800.0f,   0.0f}}
@@ -184,7 +184,7 @@ void chClam_emitSmallShellParticles(f32 position[3], s32 count){
     ParticleEmitter *pCtrl;
 
     pCtrl = partEmitMgr_newEmitter(count);
-    chClam_particalEmitterInit(pCtrl, position);
+    __chClam_particalEmitterInit(pCtrl, position);
     particleEmitter_setModel(pCtrl, ASSET_37E_MODEL_CLAM_SMALL_SHELL_PIECE);
     particleEmitter_setVelocityAndAccelerationRanges(pCtrl, &D_8038C454);
     particleEmitter_setAngularVelocityRange(pCtrl, -800.0f, -800.0f, -800.0f, 800.0f, 800.0f, 800.0f);
@@ -192,7 +192,7 @@ void chClam_emitSmallShellParticles(f32 position[3], s32 count){
     particleEmitter_emitN(pCtrl, count);
 }
 
-void chClam_emitEatencollectibleParticles(f32 position[3], enum asset_e sprite_id, s32 count){
+static void __chClam_emitEatencollectibleParticles(f32 position[3], enum asset_e sprite_id, s32 count){
     static ParticleScaleAndLifetimeRanges D_8038C484 = {
     {0.2f,  0.35f},
     {0.0f,  0.0f},
@@ -217,22 +217,22 @@ void chClam_emitEatencollectibleParticles(f32 position[3], enum asset_e sprite_i
     particleEmitter_emitN(pCtrl, count);
 }
 
-void chClam_takeDamage(ActorMarker *this_marker, ActorMarker *other_marker){
+static void __chClam_takeDamage(ActorMarker *this_marker, ActorMarker *other_marker){
     Actor *this;
 
     this = marker_getActor(this_marker);
     this->marker->collidable = FALSE;
     this->unk138_27 = TRUE;
-    chClam_playSfx(SFX_1D_HITTING_AN_ENEMY_1, 1.0f, 26000, this->position, 1500.0f, 2000.0f);
-    chClam_playSfx(SFX_115_BUZZBOMB_DEATH, 1.2f, 26000, this->position, 1500.0f, 2000.0f);
-    chClam_emitLargeShellParticles(this->position, 2);
-    chClam_emitEyeParticles(this->position, 2);
-    chClam_emitSmallShellParticles(this->position, 0xC);
+    __chClam_playSfx(SFX_1D_HITTING_AN_ENEMY_1, 1.0f, 26000, this->position, 1500.0f, 2000.0f);
+    __chClam_playSfx(SFX_115_BUZZBOMB_DEATH, 1.2f, 26000, this->position, 1500.0f, 2000.0f);
+    __chClam_emitLargeShellParticles(this->position, 2);
+    __chClam_emitEyeParticles(this->position, 2);
+    __chClam_emitSmallShellParticles(this->position, 0xC);
     func_803115C4(0xa14);
     marker_despawn(this->marker);
 }
 
-void chClam_playerDropsItem(s32 index, enum item_e item_id){
+static void __chClam_playerDropsItem(s32 index, enum item_e item_id){
     f32 position[3];
 
     player_getPosition(position);
@@ -241,7 +241,7 @@ void chClam_playerDropsItem(s32 index, enum item_e item_id){
     item_dec(item_id);
 }
 
-void chClam_attackOther(ActorMarker *this_marker, ActorMarker *other_marker){
+static void __chClam_attackOther(ActorMarker *this_marker, ActorMarker *other_marker){
     
     if(func_80297C6C() == 3) return;
 
@@ -250,13 +250,13 @@ void chClam_attackOther(ActorMarker *this_marker, ActorMarker *other_marker){
     }
 
     if(item_getCount(ITEM_D_EGGS) != 0)
-        chClam_playerDropsItem(0xe, ITEM_D_EGGS);
+        __chClam_playerDropsItem(0xe, ITEM_D_EGGS);
 
     if(item_getCount(ITEM_F_RED_FEATHER) != 0)
-        chClam_playerDropsItem(0xf, ITEM_F_RED_FEATHER);
+        __chClam_playerDropsItem(0xf, ITEM_F_RED_FEATHER);
 }
 
-void chClam_updateFunc(Actor *this){
+static void __chClam_updateFunc(Actor *this){
     ActorProp *sp4C = func_80320EB0(this->marker, 30.0f, 1);
     f32 sp48;
     s32 sp44;
@@ -269,7 +269,7 @@ void chClam_updateFunc(Actor *this){
 
     if(!this->volatile_initialized){
         this->volatile_initialized = TRUE;
-        marker_setCollisionScripts(this->marker, NULL, chClam_attackOther, chClam_takeDamage);
+        marker_setCollisionScripts(this->marker, NULL, __chClam_attackOther, __chClam_takeDamage);
     }
 
     if(this->state != 3){
@@ -293,11 +293,11 @@ void chClam_updateFunc(Actor *this){
 
     switch(this->state){
         case 1://L80387170
-            if(chClam_rotateTowardTarget(this, 140)){
+            if(__chClam_rotateTowardTarget(this, 140)){
                 subaddie_set_state_with_direction(this, 2, 0.01f, 1);
                 actor_playAnimationOnce(this);
                 animctrl_setDuration(this->animctrl, 1.0f);
-                chClam_playSfx(SFX_3F2_UNKNOWN, randf2(1.0f, 1.1f), 22000, this->position, 1500.0f, 2000.0f);
+                __chClam_playSfx(SFX_3F2_UNKNOWN, randf2(1.0f, 1.1f), 22000, this->position, 1500.0f, 2000.0f);
             }
             else{
                 animctrl_setDuration(this->animctrl, 2.0f);
@@ -308,13 +308,13 @@ void chClam_updateFunc(Actor *this){
             this->position_y += this->velocity_y;
             this->velocity_y += -5.0f;
             if(actor_animationIsAt(this, 0.63f)){
-                chClam_playSfx(SFX_80_YUMYUM_CLACK, 1.0f, 20000, this->position, 1500.0f, 2000.0f);
+                __chClam_playSfx(SFX_80_YUMYUM_CLACK, 1.0f, 20000, this->position, 1500.0f, 2000.0f);
             }
 
             if(this->position_y <= sp48){
                 this->position_y = sp48;
                 if(actor_animationIsAt(this, 0.99f) || 0.98 < animctrl_getAnimTimer(this->animctrl)){
-                    chClam_func_80386454(this);
+                    __chClam_func_80386454(this);
                 }
             }
             else{//L803872D4
@@ -330,12 +330,12 @@ void chClam_updateFunc(Actor *this){
             }
 
             if(3.0f <= this->velocity_x){
-                chClam_func_80386454(this);
+                __chClam_func_80386454(this);
                 break;
             }
 
             if(actor_animationIsAt(this, 0.8f) && 2.0f == this->velocity_x){
-                chClam_playSfx(SFX_4B_GULPING, randf2(0.8f, 0.9f), 22000, this->position, 700.0f, 2000.0f);
+                __chClam_playSfx(SFX_4B_GULPING, randf2(0.8f, 0.9f), 22000, this->position, 700.0f, 2000.0f);
                 break;
             }//L803873C4
 
@@ -343,15 +343,15 @@ void chClam_updateFunc(Actor *this){
 
             if(!this->marker->unk14_21) break;
 
-            chClam_playSfx(SFX_4C_LIP_SMACK, 1.0f, 20000, this->position, 500.0f, 2000.0f);
+            __chClam_playSfx(SFX_4C_LIP_SMACK, 1.0f, 20000, this->position, 500.0f, 2000.0f);
             func_8034A174(this->marker->unk44, 5, sp38);
 
             switch(this->unk38_31){
                 case MARKER_60_BLUE_EGG_COLLECTIBLE:
-                    chClam_emitEatencollectibleParticles(sp38, ASSET_718_SPRITE_SPARKLE_WHITE_2, 8);
+                    __chClam_emitEatencollectibleParticles(sp38, ASSET_718_SPRITE_SPARKLE_WHITE_2, 8);
                     break;
                 case MARKER_B5_RED_FEATHER_COLLECTIBLE:
-                    chClam_emitEatencollectibleParticles(sp38, ASSET_715_SPRITE_SPARKLE_RED, 8);
+                    __chClam_emitEatencollectibleParticles(sp38, ASSET_715_SPRITE_SPARKLE_RED, 8);
                     break;
             }
             break;
