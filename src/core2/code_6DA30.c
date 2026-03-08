aaa9a84e625c77c004292a5eb1dba1a1a169192e
MaikelChan <maikelchan88@gmail.com>
2026-03-08 17:25:43 +0100

    More text print documentation.

--- src/core2/code_6DA30.c
+++ src/core2/code_6DA30.c
@@ -66,7 +66,7 @@ extern BoldFontKerningData boldFontKernings[] = {
     0
 };
 
-s32 maxCharacterWidths[] = {8, 16, 16, 0};
+s32 maxFontLetterWidths[FONTS_4_MAX] = {8, 16, 16, 0};
 
 struct {
     u8 r;
@@ -489,7 +489,7 @@ void printbuffer_defrag(void){
 }
 
 //returns the pixel data and type for a given letter
-BKSpriteTextureBlock *func_802F5494(s32 letterId, s32 *fontType){
+BKSpriteTextureBlock *print_getBoldFontLetterSprite(s32 letterId, s32 *fontType){
     if(print_sCurrentFontIndex != 1 || (print_sCurrentFontIndex == 1 && letterId < 0xA)){
         *fontType = print_sFontSpriteAssets[print_sCurrentFontIndex]->type;
         return print_sFonts[print_sCurrentFontIndex][letterId].sprite;
@@ -513,28 +513,27 @@ void *print_getCurrentFontPalette(u8 letterId){
     return  print_sFonts[print_sCurrentFontIndex][letterId].palette;
 }
 
-void _printbuffer_draw_letter(char letter, f32* xPtr, f32* yPtr, f32 arg3, Gfx **gfx, Mtx **mtx, Vtx **vtx){
+void _printbuffer_draw_letter(char letter, f32* xPtr, f32* yPtr, f32 scale, Gfx **gfx, Mtx **mtx, Vtx **vtx){
     static f32 D_80380FA0;
-    
-    // u8 letter = arg0;
-    BKSpriteTextureBlock *sp214;
+
+    BKSpriteTextureBlock *letter_sprite;
     s32 sp210;
-    s32 sp20C;
-    s32 t0;
-    s8 t1;
+    s32 letter_id;
+    s32 valid_letter;
+    s8 y_offset;
     f32 x;
     f32 y;
-    f32 sp1F8;
-    s32 sp1F4; //font_type;
+    f32 letter_width;
+    s32 font_type;
 
     int i;
 
 
 
-    t0 = 0;
+    valid_letter = 0;
     x = *xPtr;
     y = *yPtr;
-    t1 = 0;
+    y_offset = 0;
 
     if(!D_80380B04 && !letter){
         D_80380FA0 = 0.0f;
@@ -543,43 +542,43 @@ void _printbuffer_draw_letter(char letter, f32* xPtr, f32* yPtr, f32 arg3, Gfx *
     switch(print_sCurrentFontIndex){
         case 0: //L802F5678
             if(letter >= '\x21' && letter < '\x5f'){
-                sp20C = letter - '\x21';
-                t0 = 1;
+                letter_id = letter - '\x21';
+                valid_letter = 1;
             }
             break;
         case 1: //L802F56A0
             if(letter < '\x80' && print_sBoldFontLetterToSpriteMap[letter] >= 0){
                 for(i = 0; boldFontKernings[i].firstLetter != 0; i++){
                     if(letter == boldFontKernings[i].secondLetter && print_sPreviousBoldLetter == boldFontKernings[i].firstLetter){
-                        t1 = boldFontKernings[i].yOffset;
+                        y_offset = boldFontKernings[i].yOffset;
                         break;
                     }
                 }//L802F5710
-                sp20C = print_sBoldFontLetterToSpriteMap[letter];
-                t0 = 1;
+                letter_id = print_sBoldFontLetterToSpriteMap[letter];
+                valid_letter = 1;
                 print_sPreviousBoldLetter = letter;
-                y += (f32)t1*arg3;
+                y += (f32)y_offset*scale;
             }//L802F5738
             break;
         case 2: //L802F5740
-            sp20C = letter;
+            letter_id = letter;
             if(D_80380B04){
-                t0 = 1;
-                sp20C += (D_80380B04 << 8) - 0x100;
+                valid_letter = 1;
+                letter_id += (D_80380B04 << 8) - 0x100;
                 D_80380B04 = 0;
             }
             else{//L802F5764
-                if(sp20C > 0 && sp20C < 0xfD)
-                    t0 = 1;
+                if(letter_id > 0 && letter_id < 0xfD)
+                    valid_letter = 1;
             }
             break;
     }//L802F5778
 
-    if(!t0 || print_sInFontFormatMode){
+    if(!valid_letter || print_sInFontFormatMode){
         print_sInFontFormatMode = FALSE;
         switch(letter){
             case ' '://802F5818
-                *xPtr += ((print_sMonospacedModeEnabled) ? maxCharacterWidths[print_sCurrentFontIndex]: maxCharacterWidths[print_sCurrentFontIndex]*0.8) * arg3;
+                *xPtr += ((print_sMonospacedModeEnabled) ? maxFontLetterWidths[print_sCurrentFontIndex]: maxFontLetterWidths[print_sCurrentFontIndex]*0.8) * scale;
                 break;
 
             case 'b': //L802F5890
@@ -679,37 +678,37 @@ void _printbuffer_draw_letter(char letter, f32* xPtr, f32* yPtr, f32 arg3, Gfx *
         }
     }
     else{//L802F5C08
-        sp214 = func_802F5494(sp20C, &sp1F4);
+        letter_sprite = print_getBoldFontLetterSprite(letter_id, &font_type);
         if (print_sShakyModeEnabled != 0) {
                x += randf2(-2.0f, 2.0f);
                y += randf2(-2.0f, 2.0f);
         }
-        sp1F8 = (print_sMonospacedModeEnabled != 0) ? maxCharacterWidths[print_sCurrentFontIndex] : sp214->x;
+        letter_width = (print_sMonospacedModeEnabled != 0) ? maxFontLetterWidths[print_sCurrentFontIndex] : letter_sprite->x;
 
         // temp_f2 = D_80380FA0;
         // phi_f2 = temp_f2;
         if (D_80380FA0 == 0.0f) {
-            D_80380FA0 = -sp1F8 * 0.5;
+            D_80380FA0 = -letter_width * 0.5;
         }
         
-        x += (D_80380FA0 + (sp1F8 - sp214->x) * 0.5);
-        y -= sp214->h*0.5;
-        sp210 = (s32)(sp214 + 1);
+        x += (D_80380FA0 + (letter_width - letter_sprite->x) * 0.5);
+        y -= letter_sprite->h*0.5;
+        sp210 = (s32)(letter_sprite + 1);
         while(sp210 % 8){
             sp210++;
         }
-        if (sp1F4 == SPRITE_TYPE_RGBA32) { 
-            gDPLoadTextureTile((*gfx)++, sp210, G_IM_FMT_RGBA, G_IM_SIZ_32b, sp214->w, sp214->h, 0, 0, sp214->x-1, sp214->y - 1, NULL, G_TX_CLAMP, G_TX_CLAMP, G_TX_NOMASK, G_TX_NOMASK, G_TX_NOLOD, G_TX_NOLOD);
-        } else if (sp1F4 == SPRITE_TYPE_IA8) {
-            gDPLoadTextureTile((*gfx)++, sp210, G_IM_FMT_IA, G_IM_SIZ_8b, sp214->w, sp214->h, 0, 0, sp214->x-1, sp214->y - 1, NULL, G_TX_CLAMP, G_TX_CLAMP, G_TX_NOMASK, G_TX_NOMASK, G_TX_NOLOD, G_TX_NOLOD);
-        } else if (sp1F4 == SPRITE_TYPE_I8) {
-            gDPLoadTextureTile((*gfx)++, sp210, G_IM_FMT_I, G_IM_SIZ_8b, sp214->w, sp214->h, 0, 0, sp214->x-1, sp214->y - 1, NULL, G_TX_CLAMP, G_TX_CLAMP, G_TX_NOMASK, G_TX_NOMASK, G_TX_NOLOD, G_TX_NOLOD);
-        } else if (sp1F4 == SPRITE_TYPE_I4) {
-            gDPLoadTextureTile_4b((*gfx)++, sp210, G_IM_FMT_I, sp214->w, sp214->h, 0, 0, sp214->x-1, sp214->y-1, NULL, G_TX_CLAMP, G_TX_CLAMP, G_TX_NOMASK, G_TX_NOMASK, G_TX_NOLOD, G_TX_NOLOD);
-        } else if (sp1F4 == SPRITE_TYPE_CI8) {
-            void * pal = print_getCurrentFontPalette(sp20C);
+        if (font_type == SPRITE_TYPE_RGBA32) { 
+            gDPLoadTextureTile((*gfx)++, sp210, G_IM_FMT_RGBA, G_IM_SIZ_32b, letter_sprite->w, letter_sprite->h, 0, 0, letter_sprite->x-1, letter_sprite->y - 1, NULL, G_TX_CLAMP, G_TX_CLAMP, G_TX_NOMASK, G_TX_NOMASK, G_TX_NOLOD, G_TX_NOLOD);
+        } else if (font_type == SPRITE_TYPE_IA8) {
+            gDPLoadTextureTile((*gfx)++, sp210, G_IM_FMT_IA, G_IM_SIZ_8b, letter_sprite->w, letter_sprite->h, 0, 0, letter_sprite->x-1, letter_sprite->y - 1, NULL, G_TX_CLAMP, G_TX_CLAMP, G_TX_NOMASK, G_TX_NOMASK, G_TX_NOLOD, G_TX_NOLOD);
+        } else if (font_type == SPRITE_TYPE_I8) {
+            gDPLoadTextureTile((*gfx)++, sp210, G_IM_FMT_I, G_IM_SIZ_8b, letter_sprite->w, letter_sprite->h, 0, 0, letter_sprite->x-1, letter_sprite->y - 1, NULL, G_TX_CLAMP, G_TX_CLAMP, G_TX_NOMASK, G_TX_NOMASK, G_TX_NOLOD, G_TX_NOLOD);
+        } else if (font_type == SPRITE_TYPE_I4) {
+            gDPLoadTextureTile_4b((*gfx)++, sp210, G_IM_FMT_I, letter_sprite->w, letter_sprite->h, 0, 0, letter_sprite->x-1, letter_sprite->y-1, NULL, G_TX_CLAMP, G_TX_CLAMP, G_TX_NOMASK, G_TX_NOMASK, G_TX_NOLOD, G_TX_NOLOD);
+        } else if (font_type == SPRITE_TYPE_CI8) {
+            void * pal = print_getCurrentFontPalette(letter_id);
             gDPLoadTLUT_pal256((*gfx)++, pal);
-            gDPLoadTextureTile((*gfx)++, sp210, G_IM_FMT_CI, G_IM_SIZ_8b, sp214->w, sp214->h, 0, 0, sp214->x-1, sp214->y-1, NULL, G_TX_CLAMP, G_TX_CLAMP, G_TX_NOMASK, G_TX_NOMASK, G_TX_NOLOD, G_TX_NOLOD);
+            gDPLoadTextureTile((*gfx)++, sp210, G_IM_FMT_CI, G_IM_SIZ_8b, letter_sprite->w, letter_sprite->h, 0, 0, letter_sprite->x-1, letter_sprite->y-1, NULL, G_TX_CLAMP, G_TX_CLAMP, G_TX_NOMASK, G_TX_NOMASK, G_TX_NOLOD, G_TX_NOLOD);
             gDPSetTextureLUT((*gfx)++, G_TT_RGBA16);
         }//L802F6570
         if (D_80380AF8 != 0) {
@@ -719,12 +718,12 @@ void _printbuffer_draw_letter(char letter, f32* xPtr, f32* yPtr, f32 arg3, Gfx *
             phi_a0 =  - MAX(1 - D_80380B0C, MIN(0, temp_t1));
             
             gDPSetTextureImage((*gfx)++, G_IM_FMT_I, G_IM_SIZ_8b, 32, &D_80380B20);
-            gDPSetTile((*gfx)++, G_IM_FMT_I, G_IM_SIZ_8b, (sp214->x + 8) >> 3, 0x0100, G_TX_LOADTILE, 0, G_TX_NOMIRROR | G_TX_CLAMP, G_TX_NOMASK, G_TX_NOLOD, G_TX_NOMIRROR | G_TX_CLAMP, G_TX_NOMASK, G_TX_NOLOD);
+            gDPSetTile((*gfx)++, G_IM_FMT_I, G_IM_SIZ_8b, (letter_sprite->x + 8) >> 3, 0x0100, G_TX_LOADTILE, 0, G_TX_NOMIRROR | G_TX_CLAMP, G_TX_NOMASK, G_TX_NOLOD, G_TX_NOMIRROR | G_TX_CLAMP, G_TX_NOMASK, G_TX_NOLOD);
             gDPLoadSync((*gfx)++);
-            gDPLoadTile((*gfx)++, G_TX_LOADTILE, 0 << G_TEXTURE_IMAGE_FRAC, (phi_a0) << G_TEXTURE_IMAGE_FRAC, (sp214->x) << G_TEXTURE_IMAGE_FRAC, (D_80380B0C - 1) << G_TEXTURE_IMAGE_FRAC);
+            gDPLoadTile((*gfx)++, G_TX_LOADTILE, 0 << G_TEXTURE_IMAGE_FRAC, (phi_a0) << G_TEXTURE_IMAGE_FRAC, (letter_sprite->x) << G_TEXTURE_IMAGE_FRAC, (D_80380B0C - 1) << G_TEXTURE_IMAGE_FRAC);
             gDPPipeSync((*gfx)++);
-            gDPSetTile((*gfx)++, G_IM_FMT_I, G_IM_SIZ_8b, ((sp214->x - 0 + 1)*G_IM_SIZ_8b_LINE_BYTES + 7) >> 3, 0x0100, 1, 0, G_TX_NOMIRROR | G_TX_CLAMP, G_TX_NOMASK, G_TX_NOLOD, G_TX_NOMIRROR | G_TX_CLAMP, G_TX_NOMASK, G_TX_NOLOD);
-            gDPSetTileSize((*gfx)++, 1, 0 << G_TEXTURE_IMAGE_FRAC, (MAX(0, temp_t1) + phi_a0) << G_TEXTURE_IMAGE_FRAC, (sp214->x) << G_TEXTURE_IMAGE_FRAC, (MAX(0, temp_t1) - (1 - D_80380B0C))<<G_TEXTURE_IMAGE_FRAC);
+            gDPSetTile((*gfx)++, G_IM_FMT_I, G_IM_SIZ_8b, ((letter_sprite->x - 0 + 1)*G_IM_SIZ_8b_LINE_BYTES + 7) >> 3, 0x0100, 1, 0, G_TX_NOMIRROR | G_TX_CLAMP, G_TX_NOMASK, G_TX_NOLOD, G_TX_NOMIRROR | G_TX_CLAMP, G_TX_NOMASK, G_TX_NOLOD);
+            gDPSetTileSize((*gfx)++, 1, 0 << G_TEXTURE_IMAGE_FRAC, (MAX(0, temp_t1) + phi_a0) << G_TEXTURE_IMAGE_FRAC, (letter_sprite->x) << G_TEXTURE_IMAGE_FRAC, (MAX(0, temp_t1) - (1 - D_80380B0C))<<G_TEXTURE_IMAGE_FRAC);
             
             // gDPLoadMultiTile((*gfx)++, &D_80380B20,)
             
@@ -737,18 +736,18 @@ void _printbuffer_draw_letter(char letter, f32* xPtr, f32* yPtr, f32 arg3, Gfx *
             f32 temp_f26;
             f32 spC0;
 
-            temp_f24 = (sp214->x - 1.0);
-            spD0 = sp214->y - 1.0;
+            temp_f24 = (letter_sprite->x - 1.0);
+            spD0 = letter_sprite->y - 1.0;
             temp_f26 = (f64) x - (f32) gFramebufferWidth * 0.5;
             spC0 = (f64)y - (f32)gFramebufferHeight*0.5 -0.5f;
             gSPVertex((*gfx)++, *vtx, 4, 0);
             for(iy = 0.0f; iy < 2.0; iy+= 1.0){
                 for(ix = 0.0f; ix < 2.0; ix += 1.0){
                     s32 s = (ix * temp_f24 * 64.0f);
-                    (*vtx)->v.ob[0] = (s16)(s32)((f64) (temp_f26 + (temp_f24 *  arg3  * ix)) * 4.0);
+                    (*vtx)->v.ob[0] = (s16)(s32)((f64) (temp_f26 + (temp_f24 *  scale  * ix)) * 4.0);
                     {
                         s32 t = (iy * spD0 * 64.0f);
-                        (*vtx)->v.ob[1] = (s16) (s32) ((f64) (spC0 + (spD0 * arg3 * iy)) * -4.0);
+                        (*vtx)->v.ob[1] = (s16) (s32) ((f64) (spC0 + (spD0 * scale * iy)) * -4.0);
                         (*vtx)->v.ob[2] = -0x14;
                         (*vtx)->v.tc[0] = s;
                         (*vtx)->v.tc[1] = t;
@@ -762,13 +761,13 @@ void _printbuffer_draw_letter(char letter, f32* xPtr, f32* yPtr, f32 arg3, Gfx *
             gSP1Quadrangle((*gfx)++, 0, 1, 3, 2, 0);
         }
         else{
-            gSPScisTextureRectangle((*gfx)++, (s32)(x*4.0f), (s32)(y*4.0f), (s32)((x + sp214->x*arg3)*4.0f), (s32)((y + sp214->y*arg3)*4.0f), 0, 0, 0, (s32)(1024.0f / arg3), (s32)(1024.0f / arg3));
+            gSPScisTextureRectangle((*gfx)++, (s32)(x*4.0f), (s32)(y*4.0f), (s32)((x + letter_sprite->x*scale)*4.0f), (s32)((y + letter_sprite->y*scale)*4.0f), 0, 0, 0, (s32)(1024.0f / scale), (s32)(1024.0f / scale));
         }
-        *xPtr += sp1F8 * arg3;
+        *xPtr += letter_width * scale;
     }
 }
 
-f32 func_802F6C90(u8 letter, f32* xPtr, f32 *yPtr, f32 arg3){
+f32 func_802F6C90(u8 letter, f32* xPtr, f32 *yPtr, f32 scale){
     s32 sp44;
     s32 i;
     bool var_v0;
@@ -792,7 +791,7 @@ f32 func_802F6C90(u8 letter, f32* xPtr, f32 *yPtr, f32 arg3){
                 print_sPreviousBoldLetter = letter;
                 sp44 = print_sBoldFontLetterToSpriteMap[letter];
                 var_v0 = TRUE;
-                sp38 += sp34 * arg3;
+                sp38 += sp34 * scale;
             }
         }
     }
@@ -801,7 +800,7 @@ f32 func_802F6C90(u8 letter, f32* xPtr, f32 *yPtr, f32 arg3){
     }
     if (!var_v0 || print_sInFontFormatMode) {
         if (letter == ' ') {
-            var_f2 = (print_sMonospacedModeEnabled) ? maxCharacterWidths[print_sCurrentFontIndex] : 0.8*maxCharacterWidths[print_sCurrentFontIndex];
+            var_f2 = (print_sMonospacedModeEnabled) ? maxFontLetterWidths[print_sCurrentFontIndex] : 0.8*maxFontLetterWidths[print_sCurrentFontIndex];
         }
         else{
             return *xPtr;
@@ -809,14 +808,14 @@ f32 func_802F6C90(u8 letter, f32* xPtr, f32 *yPtr, f32 arg3){
     }
     else {
         if(print_sMonospacedModeEnabled){
-            var_f2 = maxCharacterWidths[print_sCurrentFontIndex];
+            var_f2 = maxFontLetterWidths[print_sCurrentFontIndex];
         }
         else{
-            var_f2 = func_802F5494(sp44, &sp2C)->x;
+            var_f2 = print_getBoldFontLetterSprite(sp44, &sp2C)->x;
         }
     }
     var_f2 += (sp34 - 4);
-    *xPtr += var_f2 * arg3;
+    *xPtr += var_f2 * scale;
     
     return sp38;
 }
@@ -840,11 +839,11 @@ void printbuffer_draw(Gfx **gfx, Mtx **mtx, Vtx **vtx) {
                 _printbuffer_draw_letter(print_sCurrentPtr->fmtString[j], &_x, &_y, 1.0f, gfx, mtx, vtx);
             }
             if (D_80380B00 != 0) {
-                width = (strlen(print_sCurrentPtr->string) -1)*maxCharacterWidths[print_sCurrentFontIndex];
+                width = (strlen(print_sCurrentPtr->string) -1)*maxFontLetterWidths[print_sCurrentFontIndex];
                 gDPPipeSync((*gfx)++);
                 gDPSetPrimColor((*gfx)++, 0, 0, 0x00, 0x00, 0x00, 0x64);
                 gDPSetCombineMode((*gfx)++, G_CC_PRIMITIVE, G_CC_PRIMITIVE);
-                gDPScisFillRectangle((*gfx)++, _x - maxCharacterWidths[print_sCurrentFontIndex]/2 - 1.0f, _y - maxCharacterWidths[print_sCurrentFontIndex]/2 - 1.0f, _x + width + maxCharacterWidths[print_sCurrentFontIndex]/2, _y + maxCharacterWidths[print_sCurrentFontIndex]/2 + 1.0f);
+                gDPScisFillRectangle((*gfx)++, _x - maxFontLetterWidths[print_sCurrentFontIndex]/2 - 1.0f, _y - maxFontLetterWidths[print_sCurrentFontIndex]/2 - 1.0f, _x + width + maxFontLetterWidths[print_sCurrentFontIndex]/2, _y + maxFontLetterWidths[print_sCurrentFontIndex]/2 + 1.0f);
                 gDPPipeSync((*gfx)++);
 
             }//L802F73E8
